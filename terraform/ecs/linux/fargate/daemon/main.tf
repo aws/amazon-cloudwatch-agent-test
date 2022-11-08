1497046ba4293e0765e5b08e671c9b4138e3afae
Chena Lee <leechena@amazon.com>
2022-11-08 09:24:36 -0600

    Fix servicename flag

--- terraform/ecs/linux/fargate/daemon/main.tf
+++ terraform/ecs/linux/fargate/daemon/main.tf
@@ -146,7 +146,7 @@ resource "null_resource" "validator" {
     command = <<-EOT
       echo "Validating metrics/logs"
       cd ../../../../..
-      go test ${var.test_dir} -cwagentConfigSsmParamName=${local.cwagent_config_ssm_param_name} -clusterArn=${aws_ecs_cluster.cluster.arn} -serviceName=${aws_ecs_service.cwagent_service.name} --tags=integration
+      go test ${var.test_dir} -cwagentConfigSsmParamName=${local.cwagent_config_ssm_param_name} -clusterArn=${aws_ecs_cluster.cluster.arn} -cwagentECSServiceName=${aws_ecs_service.cwagent_service.name} --tags=integration
     EOT
   }
   depends_on = [aws_ecs_service.cwagent_service, aws_ecs_service.extra_apps_service]
