8efa964b7226d93b9fe0a5c7b43c2160760ee8c0
Chena Lee <leechena@amazon.com>
2022-11-08 09:18:18 -0600

    ListTasks in order to stop that task

---
+++ test/ecs.go
@@ -0,0 +1,28 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+//go:build integration
+// +build integration
+
+package test
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
+
+func ListTasks(clusterArn *string, serviceName *string) ([]*string, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := ecs.New(sess)
+	isOverwriteAllowed := true
+
+	listTasksOutput, err := svc.ListTasks(&ecs.ListTasksInput{
+		Cluster:     clusterArn,
+		ServiceName: serviceName,
+	})
+
+	return listTasksOutput.TaskArns, err
+}
