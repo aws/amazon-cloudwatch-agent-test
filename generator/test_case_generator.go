a5c977ba5f191e7a6e885be4b9d059b743be86fd
Khanh Nguyen <khanhntd@amazon.com>
2022-11-28 13:14:09 -0500

    Separate EC2 Advance and Basic Linux

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -20,13 +20,21 @@ var osToTestDirMap = map[string][]string{
 	"ec2_gpu": {
 		"./test/nvidia_gpu",
 	},
+	"ec2_linux_advance": {
+		"./integration/test/cloudwatchlogs",
+		"./integration/test/metrics_number_dimension",
+		"./test/metric_value_benchmark",
+		"./test/run_as_user",
+		"./test/collection_interval",
+	},
+	"ec2_linux_basic": {
+		"./integration/test/ca_bundle",
+		"./integration/test/simple_metrics_logs",
+	},
 	"ec2_linux": {
 		"./test/ca_bundle",
 		"./test/cloudwatchlogs",
 		"./test/metrics_number_dimension",
-		"./test/metric_value_benchmark",
-		"./test/run_as_user",
-		"./test/collection_interval",
 	},
 	"ec2_performance": {
 		"./test/performancetest",
