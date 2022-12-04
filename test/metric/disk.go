5f332633f5b1e2903cb90b00e11c09a4811ec3f6
Billy Zhou <billyzh@amazon.com>
2022-12-04 09:27:21 -0800

    resources any, no dimensions

--- test/metric/disk.go
+++ test/metric/disk.go
@@ -48,10 +48,5 @@ func (f *DiskMetricValueFetcher) getPluginSupportedMetric() map[string]struct{} 
 }
 
 func (f *DiskMetricValueFetcher) getMetricSpecificDimensions() []types.Dimension {
-	return []types.Dimension{
-		{
-			Name:  aws.String("test"),
-			Value: aws.String("disk-sanity"),
-		},
-	}
+	return []types.Dimension{}
 }
