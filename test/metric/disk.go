8d08e4e3da776aff6a77f21fec71ec554ed7d7ce
Billy Zhou <billyzh@amazon.com>
2022-12-04 07:53:36 -0800

    identifier='disk_sanity'

--- test/metric/disk.go
+++ test/metric/disk.go
@@ -50,6 +50,10 @@ func (f *DiskMetricValueFetcher) getPluginSupportedMetric() map[string]struct{} 
 func (f *DiskMetricValueFetcher) getMetricSpecificDimensions() []types.Dimension {
 	return []types.Dimension{
 		{
+			Name:  aws.String("path"),
+			Value: aws.String("/"),
+		},
+		{
 			Name:  aws.String("identifier"),
 			Value: aws.String("disk_sanity"),
 		},
