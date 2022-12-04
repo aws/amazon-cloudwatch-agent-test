f8442000febcaf3a553863becf7c65617afdd792
Billy Zhou <billyzh@amazon.com>
2022-12-04 09:01:09 -0800

    Deleted all metric specific dimensions

--- test/metric/disk.go
+++ test/metric/disk.go
@@ -48,14 +48,5 @@ func (f *DiskMetricValueFetcher) getPluginSupportedMetric() map[string]struct{} 
 }
 
 func (f *DiskMetricValueFetcher) getMetricSpecificDimensions() []types.Dimension {
-	return []types.Dimension{
-		{
-			Name:  aws.String("path"),
-			Value: aws.String("/"),
-		},
-		{
-			Name:  aws.String("fstype"),
-			Value: aws.String("xfs"),
-		},
-	}
+	return []types.Dimension{}
 }
