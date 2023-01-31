f3afe5f62c5313d01f80dbe98cef43ebbaf1a772
Billy Zhou <billyzh@amazon.com>
2023-01-31 18:15:02 -0500

    moved script to scripts folder

--- Makefile
+++ Makefile
@@ -9,16 +9,6 @@ install-tools:
 lint: install-tools checklicense impi
 	${LINTER} run ./...
 
-build-check:
-	mkdir -p test_binaries
-	go test -c ./test/ca_bundle -o ./test_binaries/ca_bundle.test
-	go test -c ./test/canary -o ./test_binaries/canary.test
-	go test -c ./test/cloudwatchlogs -o ./test_binaries/cloudwatchlogs.test
-	go test -c ./test/collection_interval -o ./test_binaries/collection_interval.test
-	go test -c ./test/ecs/ecs_metadata -o ./test_binaries/ecs_metadata.test
-	go test -c ./test/metric_value_benchmark -o ./test_binaries/metric_value_benchmark.test
-	go test -c ./test/metrics_number_dimension -o ./test_binaries/metrics_number_dimension.test
-	# go test -c ./test/nvidia_gpu -o ./test_binaries/nvidia_gpu.test
-	go test -c ./test/performancetest -o ./test_binaries/performance.test
-	go test -c ./test/sanity -o ./test_binaries/sanity.test
-	go test -c ./test/run_as_user -o ./test_binaries/run_as_user.test
+compile:
+	chmod +x ./scripts/*
+	./scripts/compile.sh
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,29 +1,35 @@
+mkdir -p ./test_binaries
+
 TEST_BINARIES_DIR="./test_binaries"
 
-# compiles
+# compiles a test folder
 # $1=filepath to a directory containing a go test
 compile() {
   local root=$1
   testName=${root##*/}
   echo "compiling $testName"
+  # compiles a test without running it
+  # https://pkg.go.dev/cmd/go#hdr-Test_packages
   go test -c "$root" -o "$TEST_BINARIES_DIR/$testName.test"
 }
 
-# navigates the fs to identify folders with tests
+# navigates the filesystem to identify folders with tests
 # $1=filepath of the current directory we searching through
 dfs() {
   local root=$1
   local hasTest=false
 
-  # checks that root is a directory
+  # base case: ensure that root is a directory
   if [ ! -d "$root" ]; then
     return
   fi
 
+  # look for tests
   for path in "$root"/* ; do
     if [[ $path == *_test.go ]]; then
       hasTest=true
     fi
+    # check every path
     dfs "$path"
   done
 
@@ -32,4 +38,5 @@ dfs() {
   fi
 }
 
+# entry point
 dfs "./test"
