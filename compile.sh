b02f10d5da33c2b616d4648b5daa9ac0d8c88b75
Billy Zhou <billyzh@amazon.com>
2023-01-31 17:57:53 -0500

    Add comments

--- compile.sh
+++ compile.sh
@@ -1,31 +1,35 @@
-test() {
+TEST_BINARIES_DIR="./test_binaries"
+
+# compiles
+# $1=filepath to a directory containing a go test
+compile() {
   local root=$1
   testName=${root##*/}
   echo "compiling $testName"
-  go test -c "$root" -o ./test_binaries/"$testName".test
+  go test -c "$root" -o "$TEST_BINARIES_DIR/$testName.test"
 }
 
+# navigates the fs to identify folders with tests
+# $1=filepath of the current directory we searching through
 dfs() {
   local root=$1
+  local hasTest=false
 
+  # checks that root is a directory
   if [ ! -d "$root" ]; then
     return
   fi
 
-  local hasTest=false
-  for p in "$root"/* ; do
-    local path=$p
+  for path in "$root"/* ; do
     if [[ $path == *_test.go ]]; then
-#      echo "$root"
       hasTest=true
     fi
     dfs "$path"
   done
 
   if $hasTest; then
-      test $root
+    compile "$root"
   fi
 }
 
 dfs "./test"
-
