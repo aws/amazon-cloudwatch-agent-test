5412f801d311c72a80c3c27118433765a1cfac5b
Khanh Nguyen <khanhntd@amazon.com>
2023-02-07 22:35:42 -0500

    Add ethtool validation

--- terraform/ec2/linux/vpc.tf
+++ terraform/ec2/linux/vpc.tf
@@ -15,3 +15,13 @@ data "aws_subnets" "default" {
 data "aws_security_group" "ec2_security_group" {
   name = module.common.vpc_security_group
 }
+
+resource "aws_network_interface" "test" {
+  subnet_id       = data.aws_subnets.default.ids[0]
+  security_groups = [data.aws_security_group.ec2_security_group.id]
+
+  attachment {
+    instance     = aws_instance.cwagent.id
+    device_index = 1
+  }
+}
