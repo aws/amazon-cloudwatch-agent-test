d5512621f0735dd6eb937b28a0c44c06ac89d91f
Seth L <81644108+sethAmazon@users.noreply.github.com>
2023-02-20 15:07:21 -0500

    Remove IMDSv1 Access (#136)

--- terraform/ec2/localstack/main.tf
+++ terraform/ec2/localstack/main.tf
@@ -36,6 +36,10 @@ resource "aws_instance" "integration-test" {
   key_name               = local.ssh_key_name
   iam_instance_profile   = data.aws_iam_instance_profile.cwagent_instance_profile.name
   vpc_security_group_ids = [data.aws_security_group.ec2_security_group.id]
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens  = "required"
+  }
   provisioner "remote-exec" {
     inline = [
       "cloud-init status --wait",
