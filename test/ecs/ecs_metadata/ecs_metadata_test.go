c94c32332c0f52833257df9bbfcf929e0b23910f
Andrew Huynh <andhuy@amazon.com>
2023-02-20 14:05:52 -0500

    allow egress for prometheus metrics, fix log group name

--- test/ecs/ecs_metadata/ecs_metadata_test.go
+++ test/ecs/ecs_metadata/ecs_metadata_test.go
@@ -23,7 +23,7 @@ import (
 const (
 	RetryTime = 15
 	// Log group format: https://github.com/aws/amazon-cloudwatch-agent/blob/main/translator/translate/logs/metrics_collected/prometheus/ruleLogGroupName.go#L33
-	ECSLogGroupNameFormat = "/aws/ecs/containerinsights/%s"
+	ECSLogGroupNameFormat = "/aws/ecs/containerinsights/%s/prometheus"
 	// Log stream based on job name: https://github.com/khanhntd/amazon-cloudwatch-agent/blob/ecs_metadata/integration/test/ecs/ecs_metadata/resources/extra_apps.tpl#L41
 	LogStreamName = "prometheus-redis"
 )
