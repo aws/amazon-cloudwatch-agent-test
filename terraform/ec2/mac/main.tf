9223e670bcf1855b79bd80c966411f4d5c24c6e3
Khanh Nguyen <khanhntd@amazon.com>
2023-03-08 20:23:02 -0500

    Add support for EC2 Mac

--- terraform/ec2/mac/main.tf
+++ terraform/ec2/mac/main.tf
@@ -24,51 +24,12 @@ resource "aws_key_pair" "aws_ssh_key" {
 locals {
   ssh_key_name        = var.ssh_key_name != "" ? var.ssh_key_name : aws_key_pair.aws_ssh_key[0].key_name
   private_key_content = var.ssh_key_name != "" ? var.ssh_key_value : tls_private_key.ssh_key[0].private_key_pem
-  // Canary downloads latest binary. Integration test downloads binary connect to git hash.
-  binary_uri = var.is_canary ? "${var.s3_bucket}/release/amazon_linux/${var.arc}/latest/${var.binary_name}" : "${var.s3_bucket}/integration-test/binary/${var.cwa_github_sha}/linux/${var.arc}/${var.binary_name}"
-}
-
-#####################################################################
-# Create EFS
-#####################################################################
-resource "aws_efs_file_system" "efs" {
-  creation_token = "efs-${module.common.testing_id}"
-  tags = {
-    Name = "efs-${module.common.testing_id}"
-  }
-}
-
-resource "aws_efs_mount_target" "mount" {
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = aws_instance.cwagent.subnet_id
-  security_groups = [data.aws_security_group.ec2_security_group.id]
-}
-
-resource "null_resource" "mount_efs" {
-  depends_on = [
-    aws_efs_mount_target.mount,
-    aws_instance.cwagent
-  ]
-
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
-  }
-
-  provisioner "remote-exec" {
-    # https://docs.aws.amazon.com/efs/latest/ug/mounting-fs-mount-helper-ec2-linux.html
-    inline = [
-      "sudo mkdir ~/efs-mount-point",
-      "sudo mount -t efs -o tls ${aws_efs_file_system.efs.dns_name} ~/efs-mount-point/",
-    ]
-  }
 }
 
 #####################################################################
 # Generate EC2 Instance and execute test commands
 #####################################################################
+
 resource "aws_instance" "cwagent" {
   ami                         = data.aws_ami.latest.id
   instance_type               = var.ec2_instance_type
@@ -76,6 +37,8 @@ resource "aws_instance" "cwagent" {
   iam_instance_profile        = data.aws_iam_instance_profile.cwagent_instance_profile.name
   vpc_security_group_ids      = [data.aws_security_group.ec2_security_group.id]
   associate_public_ip_address = true
+  tenancy                     = "host"
+
   metadata_options {
     http_endpoint = "enabled"
     http_tokens   = "required"
@@ -97,14 +60,8 @@ resource "null_resource" "integration_test" {
   # Prepare Integration Test
   provisioner "remote-exec" {
     inline = [
-      "echo sha ${var.cwa_github_sha}",
-      "cloud-init status --wait",
       "echo clone and install agent",
       "git clone --branch ${var.github_test_repo_branch} ${var.github_test_repo}",
-      "cd amazon-cloudwatch-agent-test",
-      "aws s3 cp s3://${local.binary_uri} .",
-      "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
-      var.install_agent,
     ]
   }
 
@@ -112,19 +69,16 @@ resource "null_resource" "integration_test" {
   provisioner "remote-exec" {
     inline = [
       "echo prepare environment",
-      "export LOCAL_STACK_HOST_NAME=${var.local_stack_host_name}",
       "export AWS_REGION=${var.region}",
       "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
       "echo run integration test",
       "cd ~/amazon-cloudwatch-agent-test",
       "echo run sanity test && go test ./test/sanity -p 1 -v",
-      "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -v"
     ]
   }
 
   depends_on = [
     aws_instance.cwagent,
-    null_resource.mount_efs
   ]
 }
 
