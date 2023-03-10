136e5058ec09143a5d55bb53797b86fa4f646ca7
Billy Zhou <billyzh@amazon.com>
2023-03-10 12:15:48 -0500

    cleanup utils

--- integration/util.go
+++ integration/util.go
@@ -1,24 +1,12 @@
 package integration
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"strings"
 	"unicode"
 )
-
-func PrettyPrint(data interface{}) {
-	pretty, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		log.Fatal("prettyPrint() failed: ", err)
-	}
-	fmt.Printf("%v\n", string(pretty))
-}
 
 func LogFatalIfError(err error) {
 	if err != nil {
@@ -40,7 +28,6 @@ func ConvertCamelToSnakeCase(camel string) string {
 			lo = hi
 		}
 	}
-	// append last word
 	words = append(words, strings.ToLower(camel[lo:]))
 	return strings.Join(words, "_")
 }
@@ -50,31 +37,3 @@ func GetRootDir() string {
 	rootDir := path.Join(wd, "../")
 	return rootDir
 }
-
-func WriteToPath(path, payload string) {
-	raw := []byte(payload)
-	err := os.WriteFile(path, raw, 0644)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func ExecuteScript(filepath string) {
-	err := exec.Command("chmod", "+x", filepath).Run()
-	LogFatalIfError(err)
-	cmd := exec.Command("/bin/sh", filepath)
-
-	stderr, err := cmd.StderrPipe()
-	cmd.Start()
-	LogFatalIfError(err)
-
-	scanner := bufio.NewScanner(stderr)
-	scanner.Split(bufio.ScanWords)
-	for scanner.Scan() {
-		m := scanner.Text()
-		fmt.Println(m)
-	}
-	cmd.Wait()
-}
-
-func DoNothing(data any) {}
