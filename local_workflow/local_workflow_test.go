f341d438b8f32753cff374fcd5ee067cf1eac9c0
Billy Zhou <billyzh@amazon.com>
2023-03-10 01:14:14 -0500

    Refactor to test suite

--- local_workflow/local_workflow_test.go
+++ local_workflow/local_workflow_test.go
@@ -2,11 +2,25 @@ package local_workflow
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
+type LocalWorkflowTestSuite struct {
+	suite.Suite
+	Config       Config
+	VarsFilepath string
+}
+
+func (suite *LocalWorkflowTestSuite) SetupTest() {
+	suite.Config = FetchConfig()
+	suite.VarsFilepath = WriteVarsFile(suite.Config)
+}
+
+func (suite *LocalWorkflowTestSuite) TestLocalWorkflow() {
+	fmt.Println(suite.VarsFilepath)
+}
+
 func TestLocalWorkflowSuite(t *testing.T) {
-	config := FetchConfig()
-	varsFilepath := WriteVarsFile(config)
-	fmt.Println(varsFilepath)
+	suite.Run(t, new(LocalWorkflowTestSuite))
 }
