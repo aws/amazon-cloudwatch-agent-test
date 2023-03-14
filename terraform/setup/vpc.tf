223850757460e25d05d56ee9403f59c82ce878a8
Billy Zhou <billyzh@amazon.com>
2023-03-14 14:03:12 -0400

    remove efs from vpc

--- terraform/setup/vpc.tf
+++ terraform/setup/vpc.tf
@@ -24,15 +24,6 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["169.254.169.254/32"]
   }
 
-  // Allow access to EFS. https://docs.aws.amazon.com/efs/latest/ug/troubleshooting-efs-mounting.html#mount-hangs-fails-timeout
-  egress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
   // Default ECS Prometheus 
   // https://github.com/aws/amazon-cloudwatch-agent-test/blob/d5105cdc461c6fcb13049cf2d38c287674d94e21/terraform/ecs/linux/default_resources/default_extra_apps.tpl
   ingress {
@@ -53,14 +44,6 @@ resource "aws_security_group" "ec2_security_group" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // NFS https://docs.aws.amazon.com/efs/latest/ug/troubleshooting-efs-mounting.html#mount-hangs-fails-timeout
-  ingress {
-    from_port   = 2049
-    to_port     = 2049
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
