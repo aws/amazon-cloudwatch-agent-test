f4cb8e5525c579c79a115d8719c6b99894bcf024
Andrew Huynh <andhuy@amazon.com>
2023-05-03 09:35:56 -0400

    try fixing connection refused error

--- terraform/eks/daemon/providers.tf
+++ terraform/eks/daemon/providers.tf
@@ -3,4 +3,9 @@ provider "aws" {
 }
 
 provider "kubernetes" {
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.name]
+  }
 }
