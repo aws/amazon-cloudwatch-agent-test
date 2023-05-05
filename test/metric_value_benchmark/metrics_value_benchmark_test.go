45128d19611eb23879c5e930b8c8f180c748db1b
Dinakar Chappa <dchappa@amazon.com>
2023-05-05 12:31:27 -0400

    Adds emf tests. Changes for integ testing

--- test/metric_value_benchmark/metrics_value_benchmark_test.go
+++ test/metric_value_benchmark/metrics_value_benchmark_test.go
@@ -61,6 +61,11 @@ func getEcsTestRunners(env *environment.MetaData) []*test_runner.ECSTestRunner {
 				RunStrategy: &test_runner.ECSAgentRunStrategy{},
 				Env:         *env,
 			},
+			{
+				testRunner:       &EMFTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}, *env},
+				agentRunStrategy: &ECSAgentRunStrategy{},
+				env:              *env,
+			},
 		}
 	}
 	return ecsTestRunners
@@ -97,7 +102,7 @@ func getEc2TestRunners(env *environment.MetaData) []*test_runner.TestRunner {
 			{TestRunner: &DiskIOTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
 			{TestRunner: &NetTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
 			{TestRunner: &EthtoolTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
-			{TestRunner: &EMFTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
+			{TestRunner: &EMFTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}, *env}},
 			{TestRunner: &SwapTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
 			{TestRunner: &ProcessesTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
 			{TestRunner: &CollectDTestRunner{test_runner.BaseTestRunner{DimensionFactory: factory}}},
