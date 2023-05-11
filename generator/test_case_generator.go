c28c9aee4a2a9e1f2058f9377eaa27951cb5742b
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 20:35:21 -0400

    uncommenting tests for linux

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -34,13 +34,13 @@ var testTypeToTestDirMap = map[string][]string{
 		"./test/nvidia_gpu",
 	},
 	"ec2_linux": {
-		//"./test/ca_bundle",
+		"./test/ca_bundle",
 		"./test/cloudwatchlogs",
-		//"./test/metrics_number_dimension",
+		"./test/metrics_number_dimension",
 		"./test/metric_value_benchmark",
 		"./test/run_as_user",
-		/* "./test/collection_interval",
-		"./test/metric_dimension", */
+		"./test/collection_interval",
+		"./test/metric_dimension",
 	},
 	/*
 		You can only place 1 mac instance on a dedicate host a single time.
