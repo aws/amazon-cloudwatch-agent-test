dc626d6592ba1b683c32b8a3b4f1ea28d50753e2
Dinakar Chappa <dchappa@amazon.com>
2023-05-17 09:51:17 -0400

    Removes commented out code

--- test/metric/dimension/emf_ecs_dimension_provider.go
+++ test/metric/dimension/emf_ecs_dimension_provider.go
@@ -14,41 +14,6 @@ func (p EFMECSDimensionProvider) IsApplicable() bool {
 }
 
 func (p EFMECSDimensionProvider) GetDimension(instruction Instruction) types.Dimension {
-	//if instruction.Key == "ClusterName" {
-	//	return types.Dimension{
-	//		Name:  aws.String("ClusterName"),
-	//		Value: aws.String( /*p.Provider.env.EcsClusterName +*/ "cluster"),
-	//	}
-	//}
-	//
-	//if instruction.Key == "ContainerInstanceId" {
-	//	//TODO currently assuming there's only one container
-	//	//containerInstances, err := awsservice.GetContainerInstances(p.Provider.env.EcsClusterArn)
-	//	//if err != nil {
-	//	//	log.Print(err)
-	//	//	return types.Dimension{}
-	//	//}
-	//
-	//	return types.Dimension{
-	//		Name:  aws.String("ContainerInstanceId"),
-	//		Value: aws.String( /*containerInstances[0].ContainerInstanceId +*/ "containerID"),
-	//	}
-	//}
-
-	//if instruction.Key == "InstanceId" {
-	//	//TODO currently assuming there's only one container
-	//	//containerInstances, err := awsservice.GetContainerInstances(p.Provider.env.EcsClusterArn)
-	//	//if err != nil {
-	//	//	log.Print(err)
-	//	//	return types.Dimension{}
-	//	//}
-	//
-	//	return types.Dimension{
-	//		Name:  aws.String("InstanceId"),
-	//		Value: aws.String( /*containerInstances[0].ContainerInstanceArn +*/ "INSTANCEID"),
-	//	}
-	//}
-
 	return types.Dimension{}
 }
 
