7952946627bd3ef30e52bcf6111ee4075a1d0e82
Jason Polanco <polajaso@amazon.com>
2023-05-18 16:15:54 -0400

    make variables not nullable

--- terraform/eks/deployment/variables.tf
+++ terraform/eks/deployment/variables.tf
@@ -4,24 +4,29 @@
 variable "region" {
   type    = string
   default = "us-west-2"
+  nullable = false
 }
 
 variable "test_dir" {
   type    = string
   default = "./test/metric_value_benchmark"
+  nullable = false
 }
 
 variable "cwagent_image_repo" {
   type    = string
   default = "public.ecr.aws/cloudwatch-agent/cloudwatch-agent"
+  nullable = false
 }
 
 variable "cwagent_image_tag" {
   type    = string
   default = "latest"
+  nullable = false
 }
 
 variable "k8s_version" {
   type    = string
   default = "1.24"
+  nullable = false
 }
