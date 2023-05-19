87c44c56b981609eebc06a0980115069eaff77f8
Dinakar Chappa <dchappa@amazon.com>
2023-05-19 16:31:55 -0400

    Adds log lines to figure out terraform dir

--- terraform/eks/daemon/emf/main.tf
+++ terraform/eks/daemon/emf/main.tf
@@ -474,7 +474,7 @@ resource "null_resource" "validator" {
   ]
   provisioner "local-exec" {
     command = <<-EOT
-      echo "Validating EKS metrics/logs"
+      echo "Validating EKS metrics/logs for EMF"
       cd ../../../..
       go test ${var.test_dir} -eksClusterName=${aws_eks_cluster.this.name} -computeType=EKS -v
     EOT
