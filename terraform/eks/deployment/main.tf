1ed9602c2a096e68aa39fa0a56daac0a78f24db8
Jason Polanco <polajaso@amazon.com>
2023-05-19 17:37:52 -0400

    typo

--- terraform/eks/deployment/main.tf
+++ terraform/eks/deployment/main.tf
@@ -294,8 +294,8 @@ resource "kubernetes_service" "redis_service" {
     name = "my-redis-metrics"
     namespace = "redis-test"
     annotations = {
-      prometheus.io/port = "9121"
-      prometheus.io/scrape = "true"
+      "prometheus.io/port" = "9121"
+      "prometheus.io/scrape" = "true"
     }
   }
   spec {
