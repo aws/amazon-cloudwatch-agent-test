71503aa28fbea759a01d3d6674655a9d51c3d526
Dinakar Chappa <dchappa@amazon.com>
2023-05-22 10:10:28 -0400

    removes function in favor of util function

--- test/emf_ecs/emf_ecs_test.go
+++ test/emf_ecs/emf_ecs_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric/dimension"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/status"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/test_runner"
-	"log"
 	"time"
 )
 
@@ -65,7 +64,7 @@ func (t *EMFECSTestRunner) validateEMFOnECSMetrics(metricName string) status.Tes
 		return testResult
 	}
 
-	if !isAllValuesGreaterThanOrEqualToExpectedValue(metricName, values, 0) {
+	if !metric.IsAllValuesGreaterThanOrEqualToExpectedValue(metricName, values, 0) {
 		return testResult
 	}
 
@@ -74,35 +73,4 @@ func (t *EMFECSTestRunner) validateEMFOnECSMetrics(metricName string) status.Tes
 
 	testResult.Status = status.SUCCESSFUL
 	return testResult
-}
-
-// isAllValuesGreaterThanOrEqualToExpectedValue will compare if the given array is larger than 0
-// and check if the average value for the array is not la
-// TODO: Moving metric_value_benchmark to validator
-// https://github.com/aws/amazon-cloudwatch-agent-test/pull/162
-func isAllValuesGreaterThanOrEqualToExpectedValue(metricName string, values []float64, expectedValue float64) bool {
-	if len(values) == 0 {
-		log.Printf("No values found %v", metricName)
-		return false
-	}
-
-	totalSum := 0.0
-	for _, value := range values {
-		if value < 0 {
-			log.Printf("Values are not all greater than or equal to zero for %s", metricName)
-			return false
-		}
-		totalSum += value
-	}
-	metricErrorBound := 0.1
-	metricAverageValue := totalSum / float64(len(values))
-	upperBoundValue := expectedValue * (1 + metricErrorBound)
-	lowerBoundValue := expectedValue * (1 - metricErrorBound)
-	if expectedValue > 0 && (metricAverageValue > upperBoundValue || metricAverageValue < lowerBoundValue) {
-		log.Printf("The average value %f for metric %s are not within bound [%f, %f]", metricAverageValue, metricName, lowerBoundValue, upperBoundValue)
-		return false
-	}
-
-	log.Printf("The average value %f for metric %s are within bound [%f, %f]", expectedValue, metricName, lowerBoundValue, upperBoundValue)
-	return true
 }
