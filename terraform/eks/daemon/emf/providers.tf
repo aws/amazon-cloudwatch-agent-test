53e205312a4959c91c964c4e7cc180f1a401a9d0
Dinakar Chappa <dchappa@amazon.com>
2023-05-22 22:51:28 -0400

    lint errors

--- terraform/eks/daemon/emf/providers.tf
+++ terraform/eks/daemon/emf/providers.tf
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: MIT
 
 provider "aws" {
-region = var.region
+  region = var.region
 }
 
 provider "kubernetes" {
-exec {
-api_version = "client.authentication.k8s.io/v1beta1"
-command     = "aws"
-args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
-}
-host                   = aws_eks_cluster.this.endpoint
-cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
-token                  = data.aws_eks_cluster_auth.this.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+  }
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
