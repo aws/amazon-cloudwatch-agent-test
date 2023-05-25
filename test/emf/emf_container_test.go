7f200fa361516ff62d396f9a8c62fe753fa43fa4
Dinakar Chappa <dchappa@amazon.com>
2023-05-25 10:58:22 -0400

    EMF_EKS integ test (#230)
    Adds testing for EMF output on EKS clusters

--- test/emf/emf_container_test.go
+++ test/emf/emf_container_test.go
@@ -1,26 +1,28 @@
 //go:build !windows
 
-package emf_ecs
+package emf
 
 import (
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric/dimension"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/status"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/test_runner"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"time"
 )
 
-type EMFECSTestRunner struct {
+type EMFTestRunner struct {
 	test_runner.BaseTestRunner
+	testName string
 }
 
-var _ test_runner.ITestRunner = (*EMFECSTestRunner)(nil)
+var _ test_runner.ITestRunner = (*EMFTestRunner)(nil)
 
-func (t *EMFECSTestRunner) Validate() status.TestGroupResult {
+func (t *EMFTestRunner) Validate() status.TestGroupResult {
 	metricsToFetch := t.GetMeasuredMetrics()
 	testResults := make([]status.TestResult, len(metricsToFetch))
 	for i, metricName := range metricsToFetch {
-		testResults[i] = t.validateEMFOnECSMetrics(metricName)
+		testResults[i] = t.validateEMFMetrics(metricName)
 	}
 
 	return status.TestGroupResult{
@@ -29,37 +31,51 @@ func (t *EMFECSTestRunner) Validate() status.TestGroupResult {
 	}
 }
 
-func (t *EMFECSTestRunner) GetTestName() string {
-	return "EMFOnECS"
+func (t *EMFTestRunner) GetTestName() string {
+	return "EMF Container Tests"
 
 }
 
-func (t *EMFECSTestRunner) GetAgentConfigFileName() string {
+func (t *EMFTestRunner) GetAgentConfigFileName() string {
 	return "./resources/config.json"
 }
 
-func (t *EMFECSTestRunner) GetAgentRunDuration() time.Duration {
+func (t *EMFTestRunner) GetAgentRunDuration() time.Duration {
 	return 3 * time.Minute
 }
 
-func (t *EMFECSTestRunner) GetMeasuredMetrics() []string {
+func (t *EMFTestRunner) GetMeasuredMetrics() []string {
 	return []string{"EMFCounter"}
 }
 
-func (t *EMFECSTestRunner) validateEMFOnECSMetrics(metricName string) status.TestResult {
+func (t *EMFTestRunner) validateEMFMetrics(metricName string) status.TestResult {
+	namespace := ""
+	var dims []types.Dimension
+	var failed []dimension.Instruction
+	if t.testName == "EMF_ECS" {
+		namespace = "EMFECSNameSpace"
+		dims, failed = t.DimensionFactory.GetDimensions([]dimension.Instruction{
+			{
+				Key:   "InstanceName",
+				Value: dimension.UnknownDimensionValue(),
+			},
+		})
+	}
+	if t.testName == "EMF_EKS" {
+		namespace = "EMFEKSNameSpace"
+		dims, failed = t.DimensionFactory.GetDimensions([]dimension.Instruction{})
+	}
 	testResult := status.TestResult{
 		Name:   metricName,
 		Status: status.FAILED,
 	}
-
-	dims, failed := t.DimensionFactory.GetDimensions([]dimension.Instruction{})
 
 	if len(failed) > 0 {
 		return testResult
 	}
 
 	fetcher := metric.MetricValueFetcher{}
-	values, err := fetcher.Fetch("EMFNameSpace", metricName, dims, metric.AVERAGE, metric.HighResolutionStatPeriod)
+	values, err := fetcher.Fetch(namespace, metricName, dims, metric.AVERAGE, metric.HighResolutionStatPeriod)
 	if err != nil {
 		return testResult
 	}
