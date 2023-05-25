ddfd684eb1ed89a7cc9042cbddbcc7a8656d5cee
Hyunsoo Kim <hsookim@amazon.com>
2023-05-24 22:43:59 -0400

    path fix

--- terraform/ec2/win/variable.tf
+++ terraform/ec2/win/variable.tf
@@ -53,7 +53,7 @@ variable "test_name" {
 
 variable "test_dir" {
   type    = string
-  default = "../../../test/feature/windows"
+  default = "./test/feature/windows"
 }
 
 variable "github_test_repo" {
