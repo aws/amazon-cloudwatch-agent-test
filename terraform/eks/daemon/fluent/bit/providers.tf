98784bac71a17987fcb8b2f045fd5eb810fbecd1
Hyunsoo Kim <884273+movence@users.noreply.github.com>
2023-05-28 12:11:39 -0400

    fix module vars

--- terraform/eks/daemon/fluent/bit/providers.tf
+++ terraform/eks/daemon/fluent/bit/providers.tf
@@ -9,9 +9,9 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+    args        = ["eks", "get-token", "--cluster-name", module.fluent_common.cluster_name]
   }
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = module.fluent_common.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.fluent_common.cluster_cert)
+  token                  = module.fluent_common.cluster_auth_token
 }
