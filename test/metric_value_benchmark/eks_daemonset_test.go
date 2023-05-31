91177e5f3e7b3e40d55874e2ba311485e3aa0ba5
Hyunsoo Kim <hsookim@amazon.com>
2023-05-31 16:20:30 -0400

    add all supported metrics for container insights with EKS

--- test/metric_value_benchmark/eks_daemonset_test.go
+++ test/metric_value_benchmark/eks_daemonset_test.go
@@ -150,14 +150,31 @@ func (e *EKSDaemonTestRunner) GetAgentRunDuration() time.Duration {
 
 func (e *EKSDaemonTestRunner) GetMeasuredMetrics() []string {
 	return []string{
+		"cluster_failed_node_count",
+		"cluster_node_count",
+		"namespace_number_of_running_pods",
+		"node_cpu_limit",
 		"node_cpu_reserved_capacity",
+		"node_cpu_usage_total",
 		"node_cpu_utilization",
-		"node_network_total_bytes",
 		"node_filesystem_utilization",
-		"node_number_of_running_pods",
-		"node_number_of_running_containers",
-		"node_memory_utilization",
+		"node_memory_limit",
 		"node_memory_reserved_capacity",
+		"node_memory_utilization",
+		"node_memory_working_set",
+		"node_network_total_bytes",
+		"node_number_of_running_containers",
+		"node_number_of_running_pods",
+		"pod_cpu_reserved_capacity",
+		"pod_cpu_utilization",
+		"pod_cpu_utilization_over_pod_limit",
+		"pod_memory_reserved_capacity",
+		"pod_memory_utilization",
+		"pod_memory_utilization_over_pod_limit",
+		"pod_network_rx_bytes",
+		"pod_network_tx_bytes",
+		"pod_number_of_container_restarts",
+		"service_number_of_running_pods",
 	}
 }
 
