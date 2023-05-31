86a254ad7e093325fbb2c9087375cdc46ab32e80
Hyunsoo Kim <hsookim@amazon.com>
2023-05-31 15:21:55 -0400

    fix missing param

--- test/fips/fips_test.go
+++ test/fips/fips_test.go
@@ -35,7 +35,7 @@ func TestFIPS(t *testing.T) {
 	factory := dimension.GetDimensionFactory(*env)
 
 	common.CopyFile(agentConfigLocalPath, agentConfigPath)
-	err := common.StartAgent(agentConfigPath, false)
+	err := common.StartAgent(agentConfigPath, false, false)
 	if err != nil {
 		log.Printf("Agent failed to start due to err=%v\n", err)
 	}
