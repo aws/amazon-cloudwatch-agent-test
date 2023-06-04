218ce990fd67f1e74643c682f08219ec94074d0c
Hyunsoo Kim <hsookim@amazon.com>
2023-06-03 23:37:16 -0400

    conf map no indentation

--- terraform/eks/daemon/fluent/d/main.tf
+++ terraform/eks/daemon/fluent/d/main.tf
@@ -71,321 +71,321 @@ resource "kubernetes_config_map" "fluentd_config" {
   }
   data = {
     "kubernetes.conf" = <<EOF
-    kubernetes.conf
+kubernetes.conf
     EOF
     "fluent.conf"     = <<EOF
-    @include containers.conf
-    @include systemd.conf
-    @include host.conf
+@include containers.conf
+@include systemd.conf
+@include host.conf
 
-    <match fluent.**>
-      @type null
-    </match>
+<match fluent.**>
+  @type null
+</match>
     EOF
     "containers.conf" = <<EOF
-    <source>
-      @type tail
-      @id in_tail_container_logs
-      @label @containers
-      path /var/log/containers/*.log
-      exclude_path ["/var/log/containers/cloudwatch-agent*", "/var/log/containers/fluentd*"]
-      pos_file /var/log/fluentd-containers.log.pos
-      tag *
-      read_from_head true
-      <parse>
-        @type json
-        time_format %Y-%m-%dT%H:%M:%S.%N%:z
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_container_logs
+  @label @containers
+  path /var/log/containers/*.log
+  exclude_path ["/var/log/containers/cloudwatch-agent*", "/var/log/containers/fluentd*"]
+  pos_file /var/log/fluentd-containers.log.pos
+  tag *
+  read_from_head true
+  <parse>
+    @type "#{ENV['FLUENT_CONTAINER_TAIL_PARSER_TYPE'] || 'json'}"
+    time_format %Y-%m-%dT%H:%M:%S.%N%:z
+  </parse>
+</source>
 
-    <source>
-      @type tail
-      @id in_tail_cwagent_logs
-      @label @cwagentlogs
-      path /var/log/containers/cloudwatch-agent*
-      pos_file /var/log/cloudwatch-agent.log.pos
-      tag *
-      read_from_head true
-      <parse>
-        @type json
-        time_format %Y-%m-%dT%H:%M:%S.%NZ
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_cwagent_logs
+  @label @cwagentlogs
+  path /var/log/containers/cloudwatch-agent*
+  pos_file /var/log/cloudwatch-agent.log.pos
+  tag *
+  read_from_head true
+  <parse>
+    @type json
+    time_format %Y-%m-%dT%H:%M:%S.%NZ
+  </parse>
+</source>
 
-    <source>
-      @type tail
-      @id in_tail_fluentd_logs
-      @label @fluentdlogs
-      path /var/log/containers/fluentd*
-      pos_file /var/log/fluentd.log.pos
-      tag *
-      read_from_head true
-      <parse>
-        @type json
-        time_format %Y-%m-%dT%H:%M:%S.%NZ
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_fluentd_logs
+  @label @fluentdlogs
+  path /var/log/containers/fluentd*
+  pos_file /var/log/fluentd.log.pos
+  tag *
+  read_from_head true
+  <parse>
+    @type json
+    time_format %Y-%m-%dT%H:%M:%S.%NZ
+  </parse>
+</source>
 
-    <label @fluentdlogs>
-      <filter **>
-        @type kubernetes_metadata
-        @id filter_kube_metadata_fluentd
-        watch false
-      </filter>
+<label @fluentdlogs>
+  <filter **>
+    @type kubernetes_metadata
+    @id filter_kube_metadata_fluentd
+    watch false
+  </filter>
 
-      <filter **>
-        @type record_transformer
-        @id filter_fluentd_stream_transformer
-        <record>
-          stream_name $${tag_parts[3]}
-        </record>
-      </filter>
+  <filter **>
+    @type record_transformer
+    @id filter_fluentd_stream_transformer
+    <record>
+      stream_name $${tag_parts[3]}
+    </record>
+  </filter>
 
-      <match **>
-        @type relabel
-        @label @NORMAL
-      </match>
-    </label>
+  <match **>
+    @type relabel
+    @label @NORMAL
+  </match>
+</label>
 
-    <label @containers>
-      <filter **>
-        @type kubernetes_metadata
-        @id filter_kube_metadata
-        watch false
-      </filter>
+<label @containers>
+  <filter **>
+    @type kubernetes_metadata
+    @id filter_kube_metadata
+    watch false
+  </filter>
 
-      <filter **>
-        @type record_transformer
-        @id filter_containers_stream_transformer
-        <record>
-          stream_name $${tag_parts[3]}
-        </record>
-      </filter>
+  <filter **>
+    @type record_transformer
+    @id filter_containers_stream_transformer
+    <record>
+      stream_name $${tag_parts[3]}
+    </record>
+  </filter>
 
-      <filter **>
-        @type concat
-        key log
-        multiline_start_regexp /^\S/
-        separator ""
-        flush_interval 5
-        timeout_label @NORMAL
-      </filter>
+  <filter **>
+    @type concat
+    key log
+    multiline_start_regexp /^\S/
+    separator ""
+    flush_interval 5
+    timeout_label @NORMAL
+  </filter>
 
-      <match **>
-        @type relabel
-        @label @NORMAL
-      </match>
-    </label>
+  <match **>
+    @type relabel
+    @label @NORMAL
+  </match>
+</label>
 
-    <label @cwagentlogs>
-      <filter **>
-        @type kubernetes_metadata
-        @id filter_kube_metadata_cwagent
-        watch false
-      </filter>
+<label @cwagentlogs>
+  <filter **>
+    @type kubernetes_metadata
+    @id filter_kube_metadata_cwagent
+    watch false
+  </filter>
 
-      <filter **>
-        @type record_transformer
-        @id filter_cwagent_stream_transformer
-        <record>
-          stream_name $${tag_parts[3]}
-        </record>
-      </filter>
+  <filter **>
+    @type record_transformer
+    @id filter_cwagent_stream_transformer
+    <record>
+      stream_name $${tag_parts[3]}
+    </record>
+  </filter>
 
-      <filter **>
-        @type concat
-        key log
-        multiline_start_regexp /^\d{4}[-/]\d{1,2}[-/]\d{1,2}/
-        separator ""
-        flush_interval 5
-        timeout_label @NORMAL
-      </filter>
+  <filter **>
+    @type concat
+    key log
+    multiline_start_regexp /^\d{4}[-/]\d{1,2}[-/]\d{1,2}/
+    separator ""
+    flush_interval 5
+    timeout_label @NORMAL
+  </filter>
 
-      <match **>
-        @type relabel
-        @label @NORMAL
-      </match>
-    </label>
+  <match **>
+    @type relabel
+    @label @NORMAL
+  </match>
+</label>
 
-    <label @NORMAL>
-      <match **>
-        @type cloudwatch_logs
-        @id out_cloudwatch_logs_containers
-        region "${var.region}"
-        log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/application"
-        log_stream_name_key stream_name
-        remove_log_stream_name_key true
-        auto_create_stream true
-        <buffer>
-          flush_interval 5
-          chunk_limit_size 2m
-          queued_chunks_limit_size 32
-          retry_forever true
-        </buffer>
-      </match>
-    </label>
+<label @NORMAL>
+  <match **>
+    @type cloudwatch_logs
+    @id out_cloudwatch_logs_containers
+    region "${var.region}"
+    log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/application"
+    log_stream_name_key stream_name
+    remove_log_stream_name_key true
+    auto_create_stream true
+    <buffer>
+      flush_interval 5
+      chunk_limit_size 2m
+      queued_chunks_limit_size 32
+      retry_forever true
+    </buffer>
+  </match>
+</label>
   EOF
     "systemd.conf"    = <<EOF
-    <source>
-      @type systemd
-      @id in_systemd_kubelet
-      @label @systemd
-      filters [{ "_SYSTEMD_UNIT": "kubelet.service" }]
-      <entry>
-        field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
-        field_map_strict true
-      </entry>
-      path /var/log/journal
-      <storage>
-        @type local
-        persistent true
-        path /var/log/fluentd-journald-kubelet-pos.json
-      </storage>
-      read_from_head true
-      tag kubelet.service
-    </source>
+<source>
+  @type systemd
+  @id in_systemd_kubelet
+  @label @systemd
+  filters [{ "_SYSTEMD_UNIT": "kubelet.service" }]
+  <entry>
+    field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
+    field_map_strict true
+  </entry>
+  path /var/log/journal
+  <storage>
+    @type local
+    persistent true
+    path /var/log/fluentd-journald-kubelet-pos.json
+  </storage>
+  read_from_head true
+  tag kubelet.service
+</source>
 
-    <source>
-      @type systemd
-      @id in_systemd_kubeproxy
-      @label @systemd
-      filters [{ "_SYSTEMD_UNIT": "kubeproxy.service" }]
-      <entry>
-        field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
-        field_map_strict true
-      </entry>
-      path /var/log/journal
-      <storage>
-        @type local
-        persistent true
-        path /var/log/fluentd-journald-kubeproxy-pos.json
-      </storage>
-      read_from_head true
-      tag kubeproxy.service
-    </source>
+<source>
+  @type systemd
+  @id in_systemd_kubeproxy
+  @label @systemd
+  filters [{ "_SYSTEMD_UNIT": "kubeproxy.service" }]
+  <entry>
+    field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
+    field_map_strict true
+  </entry>
+  path /var/log/journal
+  <storage>
+    @type local
+    persistent true
+    path /var/log/fluentd-journald-kubeproxy-pos.json
+  </storage>
+  read_from_head true
+  tag kubeproxy.service
+</source>
 
-    <source>
-      @type systemd
-      @id in_systemd_docker
-      @label @systemd
-      filters [{ "_SYSTEMD_UNIT": "docker.service" }]
-      <entry>
-        field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
-        field_map_strict true
-      </entry>
-      path /var/log/journal
-      <storage>
-        @type local
-        persistent true
-        path /var/log/fluentd-journald-docker-pos.json
-      </storage>
-      read_from_head true
-      tag docker.service
-    </source>
+<source>
+  @type systemd
+  @id in_systemd_docker
+  @label @systemd
+  filters [{ "_SYSTEMD_UNIT": "docker.service" }]
+  <entry>
+    field_map {"MESSAGE": "message", "_HOSTNAME": "hostname", "_SYSTEMD_UNIT": "systemd_unit"}
+    field_map_strict true
+  </entry>
+  path /var/log/journal
+  <storage>
+    @type local
+    persistent true
+    path /var/log/fluentd-journald-docker-pos.json
+  </storage>
+  read_from_head true
+  tag docker.service
+</source>
 
-    <label @systemd>
-      <filter **>
-        @type kubernetes_metadata
-        @id filter_kube_metadata_systemd
-        watch false
-      </filter>
+<label @systemd>
+  <filter **>
+    @type kubernetes_metadata
+    @id filter_kube_metadata_systemd
+    watch false
+  </filter>
 
-      <filter **>
-        @type record_transformer
-        @id filter_systemd_stream_transformer
-        <record>
-          stream_name $${tag}-$${record["hostname"]}
-        </record>
-      </filter>
+  <filter **>
+    @type record_transformer
+    @id filter_systemd_stream_transformer
+    <record>
+      stream_name $${tag}-$${record["hostname"]}
+    </record>
+  </filter>
 
-      <match **>
-        @type cloudwatch_logs
-        @id out_cloudwatch_logs_systemd
-        region "${var.region}"
-        log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/dataplane"
-        log_stream_name_key stream_name
-        auto_create_stream true
-        remove_log_stream_name_key true
-        <buffer>
-          flush_interval 5
-          chunk_limit_size 2m
-          queued_chunks_limit_size 32
-          retry_forever true
-        </buffer>
-      </match>
-    </label>
+  <match **>
+    @type cloudwatch_logs
+    @id out_cloudwatch_logs_systemd
+    region "${var.region}"
+    log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/dataplane"
+    log_stream_name_key stream_name
+    auto_create_stream true
+    remove_log_stream_name_key true
+    <buffer>
+      flush_interval 5
+      chunk_limit_size 2m
+      queued_chunks_limit_size 32
+      retry_forever true
+    </buffer>
+  </match>
+</label>
     EOF
     "host.conf"       = <<EOF
-    <source>
-      @type tail
-      @id in_tail_dmesg
-      @label @hostlogs
-      path /var/log/dmesg
-      pos_file /var/log/dmesg.log.pos
-      tag host.dmesg
-      read_from_head true
-      <parse>
-        @type syslog
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_dmesg
+  @label @hostlogs
+  path /var/log/dmesg
+  pos_file /var/log/dmesg.log.pos
+  tag host.dmesg
+  read_from_head true
+  <parse>
+    @type syslog
+  </parse>
+</source>
 
-    <source>
-      @type tail
-      @id in_tail_secure
-      @label @hostlogs
-      path /var/log/secure
-      pos_file /var/log/secure.log.pos
-      tag host.secure
-      read_from_head true
-      <parse>
-        @type syslog
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_secure
+  @label @hostlogs
+  path /var/log/secure
+  pos_file /var/log/secure.log.pos
+  tag host.secure
+  read_from_head true
+  <parse>
+    @type syslog
+  </parse>
+</source>
 
-    <source>
-      @type tail
-      @id in_tail_messages
-      @label @hostlogs
-      path /var/log/messages
-      pos_file /var/log/messages.log.pos
-      tag host.messages
-      read_from_head true
-      <parse>
-        @type syslog
-      </parse>
-    </source>
+<source>
+  @type tail
+  @id in_tail_messages
+  @label @hostlogs
+  path /var/log/messages
+  pos_file /var/log/messages.log.pos
+  tag host.messages
+  read_from_head true
+  <parse>
+    @type syslog
+  </parse>
+</source>
 
-    <label @hostlogs>
-      <filter **>
-        @type kubernetes_metadata
-        @id filter_kube_metadata_host
-        watch false
-      </filter>
+<label @hostlogs>
+  <filter **>
+    @type kubernetes_metadata
+    @id filter_kube_metadata_host
+    watch false
+  </filter>
 
-      <filter **>
-        @type record_transformer
-        @id filter_containers_stream_transformer_host
-        <record>
-          stream_name $${tag}-$${record["host"]}
-        </record>
-      </filter>
+  <filter **>
+    @type record_transformer
+    @id filter_containers_stream_transformer_host
+    <record>
+      stream_name $${tag}-$${record["host"]}
+    </record>
+  </filter>
 
-      <match host.**>
-        @type cloudwatch_logs
-        @id out_cloudwatch_logs_host_logs
-        region "${var.region}"
-        log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/host"
-        log_stream_name_key stream_name
-        remove_log_stream_name_key true
-        auto_create_stream true
-        <buffer>
-          flush_interval 5
-          chunk_limit_size 2m
-          queued_chunks_limit_size 32
-          retry_forever true
-        </buffer>
-      </match>
-    </label>
+  <match host.**>
+    @type cloudwatch_logs
+    @id out_cloudwatch_logs_host_logs
+    region "${var.region}"
+    log_group_name "/aws/containerinsights/${module.fluent_common.cluster_name}/host"
+    log_stream_name_key stream_name
+    remove_log_stream_name_key true
+    auto_create_stream true
+    <buffer>
+      flush_interval 5
+      chunk_limit_size 2m
+      queued_chunks_limit_size 32
+      retry_forever true
+    </buffer>
+  </match>
+</label>
     EOF
   }
 }
@@ -562,10 +562,6 @@ resource "null_resource" "validator" {
     command = <<-EOT
       echo "Validating EKS fluentd logs"
       cd ../../../../..
-      echo "${module.fluent_common.cluster_name}"
-      echo "${module.fluent_common.cluster_endpoint}"
-      echo "${module.fluent_common.cluster_cert}"
-      echo "${module.fluent_common.cluster_auth_token}"
       go test ${var.test_dir} -eksClusterName=${module.fluent_common.cluster_name} -computeType=EKS -v -eksDeploymentStrategy=DAEMON
     EOT
   }
