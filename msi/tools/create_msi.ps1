f7b0c96f2a4565145c33a6224bff310456521766
Adam <90734270+adam-mateen@users.noreply.github.com>
2023-06-06 12:51:27 -0500

    Allow alternate S3 upload paths.

--- msi/tools/create_msi.ps1
+++ msi/tools/create_msi.ps1
@@ -7,5 +7,10 @@ candle.exe -ext WixUtilExtension.dll ./amazon-cloudwatch-agent.wxs
 light.exe -ext WixUtilExtension.dll ./amazon-cloudwatch-agent.wixobj
 
 # upload to s3
-aws s3 cp ./amazon-cloudwatch-agent.msi "s3://$bucket/integration-test/packaging/$version/amazon-cloudwatch-agent.msi"
-Write-Host "s3 for msi is s3://$bucket/integration-test/packaging/$version/amazon-cloudwatch-agent.msi"
+$bucketPath = "s3://$bucket/integration-test/packaging/$version/amazon-cloudwatch-agent.msi"
+if ($version -eq "") {
+    # This is a prod, nonprod, or nightly build.
+    $bucketPath = "s3://$bucket/$version/amazon-cloudwatch-agent.msi"
+}
+aws s3 cp ./amazon-cloudwatch-agent.msi $bucketPath
+Write-Host "s3 for msi is $bucketPath"
