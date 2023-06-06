8fd60d3b1139c5b14f5d0b3bcb9260f3763b78f3
Hyunsoo Kim <hsookim@amazon.com>
2023-06-06 14:37:31 -0400

    update policy doc

--- terraform/ec2/creds/main.tf
+++ terraform/ec2/creds/main.tf
@@ -39,45 +39,14 @@ locals {
 #####################################################################
 resource "aws_iam_role" "assume_role" {
   name = "cwa-iam-test-assume-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_trust_policy.json
   inline_policy {
     name = "cwa-iam-test-assume-role-inline"
-
-    policy = jsonencode({
-      Version   = "2012-10-17"
-      Statement = [
-        {
-          Action   = [
-            "cloudwatch:PutMetricData",
-            "cloudwatch:ListMetrics",
-            "cloudwatch:GetMetricStatistics",
-            "cloudwatch:GetMetricData",
-            "logs:PutRetentionPolicy",
-            "logs:PutLogEvents",
-            "logs:GetLogEvents",
-            "logs:DescribeLogStreams",
-            "logs:DescribeLogGroups",
-            "logs:DeleteLogStream",
-            "logs:DeleteLogGroup",
-            "logs:CreateLogStream",
-            "logs:CreateLogGroup",
-            "ssm:List*",
-            "ssm:Get*",
-            "ssm:Describe*",
-            "s3:PutObject",
-            "s3:ListBucket",
-            "s3:GetObjectAcl",
-            "s3:GetObject",
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
+    policy = data.aws_iam_policy_document.assume_role_policy.json
   }
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "assume_role_trust_policy" {
   statement {
     effect    = "Allow"
     actions   = ["sts:AssumeRole"]
@@ -94,6 +63,35 @@ data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     effect    = "Allow"
     actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "cloudwatch:PutMetricData",
+      "cloudwatch:ListMetrics",
+      "cloudwatch:GetMetricStatistics",
+      "cloudwatch:GetMetricData",
+      "logs:PutRetentionPolicy",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:DescribeLogGroups",
+      "logs:DeleteLogStream",
+      "logs:DeleteLogGroup",
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "ssm:List*",
+      "ssm:Get*",
+      "ssm:Describe*",
+      "s3:PutObject",
+      "s3:ListBucket",
+      "s3:GetObjectAcl",
+      "s3:GetObject",
+    ]
     resources = ["*"]
   }
 }
