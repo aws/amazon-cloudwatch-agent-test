0a88df9695a21261fb155570deb531f91783505e
Hyunsoo Kim <hsookim@amazon.com>
2023-06-07 21:13:06 -0400

    command quotes

--- test/assume_role/role_unix_test.go
+++ test/assume_role/role_unix_test.go
@@ -21,7 +21,7 @@ func getCommands(roleArn string) []string {
 		"mkdir -p " + credsDir,
 		"printf '[default]\naws_access_key_id=%s\naws_secret_access_key=%s\naws_session_token=%s' $(aws sts assume-role --role-arn " + roleArn + " --role-session-name test --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text) | tee " + credsDir + "/credentials>/dev/null",
 		"printf '[default]\nregion = us-west-2' > " + credsDir + "/config",
-		"printf '[credentials]\n  shared_credential_profile = default\n  shared_credential_file = " + credsDir + "/credentials' | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/common-config.toml>/dev/null",
+		"printf '[credentials]\n  shared_credential_profile = \"default\"\n  shared_credential_file = \"" + credsDir + "/credentials\"' | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/common-config.toml>/dev/null",
 	}
 }
 
