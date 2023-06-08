ffd90c78d2a3c8a72c0017e5a53d83511a86bcc3
Hyunsoo Kim <hsookim@amazon.com>
2023-06-07 20:54:52 -0400

    fix quote error

--- test/assume_role/role_unix_test.go
+++ test/assume_role/role_unix_test.go
@@ -18,8 +18,8 @@ const (
 
 func getCommands(roleArn string) []string {
 	return []string{
-		"mkdir " + credsDir,
-		"printf '[default]\naws_access_key_id=%s\naws_secret_access_key=%s\naws_session_token=%s $(aws sts assume-role --role-arn " + roleArn + " --role-session-name test --query \"Credentials.[AccessKeyId,SecretAccessKey,SessionToken]\" --output text)' | tee " + credsDir + "/credentials>/dev/null",
+		"mkdir -p " + credsDir,
+		"printf '[default]\naws_access_key_id=%s\naws_secret_access_key=%s\naws_session_token=%s' $(aws sts assume-role --role-arn " + roleArn + " --role-session-name test --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text) | tee " + credsDir + "/credentials>/dev/null",
 		"printf '[default]\nregion = us-west-2' > " + credsDir + "/config",
 		"printf '[credentials]\n  shared_credential_profile = default\n  shared_credential_file = " + credsDir + "/credentials' | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/common-config.toml>/dev/null",
 	}
