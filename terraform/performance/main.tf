2d9b866b81f8dbc7bbdf6e39fc794e2204d8afe6
Chad Patel <patchad@amazon.com>
2023-06-14 15:30:15 -0500

    try and add some logging

--- terraform/performance/main.tf
+++ terraform/performance/main.tf
@@ -75,9 +75,21 @@ resource "null_resource" "install_binaries" {
     host        = aws_instance.cwagent.public_dns
   }
 
-  command = "echo 'CHAD!'"
-  command = "echo $(pwd)"
-  command = "echo $(ls)"
+  provisioner "local-exec" {
+    command = "echo 'CHAD!'"
+  }
+
+  provisioner "local-exec" {
+    command = "echo $(pwd)"
+  }
+
+  provisioner "local-exec" {
+    command = "echo $(ls)"
+  }
+
+  provisioner "local-exec" {
+    command = "echo $(ls ../..)"
+  }
 
   provisioner "file" {
     source      = module.validator.agent_config
