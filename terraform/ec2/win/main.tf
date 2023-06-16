41d323c44d40097829c114aedd5bf35abfd5f0a5
Dinakar Chappa <dchappa@amazon.com>
2023-06-16 09:19:40 -0400

    changes agent config

--- terraform/ec2/win/main.tf
+++ terraform/ec2/win/main.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "cwagent" {
 }
 
 data "local_file" "input" {
-  filename = module.validator.agent_config
+  filename = "../${module.validator.agent_config}"
 }
 
 # Size of windows json is too large thus can't use standard tier
