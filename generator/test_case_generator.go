11ab1f56196eed56c30031a770cdf1c75295f1cf
Pooja Reddy Nathala <poojardy@amazon.com>
2023-06-20 14:31:40 -0400

    reverted os specific test

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -95,7 +95,7 @@ var testTypeToTestConfig = map[string][]testConfig{
 		{testDir: "../../../test/feature/mac"},
 	},
 	"ec2_windows": {
-		{testDir: "../../../test/acceptance", targets: map[string]map[string]struct{}{"os": {"win-2022": {}}}},
+		{testDir: "../../../test/acceptance"},
 	},
 	"ec2_performance": {
 		{testDir: "../../test/performance/emf"},
