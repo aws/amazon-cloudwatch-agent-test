a3a5bf341f497ee211feb49decd87b54287bafe4
Dinakar Chappa <dchappa@amazon.com>
2023-06-20 15:47:53 -0400

    Adds comma

--- terraform/ec2/win/main.tf
+++ terraform/ec2/win/main.tf
@@ -113,7 +113,7 @@ resource "null_resource" "integration_test_setup" {
       "powershell.exe -Command \"Start-Sleep -s 60\"",
 #      "powershell.exe -Command \"Import-Module $env:ChocolateyInstall\\helpers\\chocolateyProfile.psm1\"",
 #      "powershell.exe -Command \"refreshenv\"",
-      "powershell.exe -Command \"Start-Process -FilePath \"C:\\Program Files\\OpenJDK\\jdk-15.0.2\\bin\\java.exe\" -ArgumentList \"-javaagent:C:\\jmx_workload\\jmx_prometheus_javaagent-0.12.0.jar=9404:C:\\jmx_workload\\exporter_config.yaml -cp C:\\jmx_workload\\SampleJavaApplication-1.0-SNAPSHOT.jar com.gubupt.sample.app.App\""
+      "powershell.exe -Command \"Start-Process -FilePath \"C:\\Program Files\\OpenJDK\\jdk-15.0.2\\bin\\java.exe\" -ArgumentList \"-javaagent:C:\\jmx_workload\\jmx_prometheus_javaagent-0.12.0.jar=9404:C:\\jmx_workload\\exporter_config.yaml -cp C:\\jmx_workload\\SampleJavaApplication-1.0-SNAPSHOT.jar com.gubupt.sample.app.App\"",
       "start /wait msiexec /i amazon-cloudwatch-agent.msi /norestart /qb-"
     ]
   }
