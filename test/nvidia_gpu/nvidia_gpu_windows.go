73874f2cee659ca301c819819f29fe72e0650f07
Hyunsoo Kim <hsookim@amazon.com>
2023-06-21 21:10:32 -0400

    fix duplicate declaration

--- test/nvidia_gpu/nvidia_gpu_windows.go
+++ test/nvidia_gpu/nvidia_gpu_windows.go
@@ -27,12 +27,12 @@ const (
 )
 
 var (
-	envMetaDataStrings              = &(environment.MetaDataStrings{})
+	envMetaDataStringsWindows       = &(environment.MetaDataStrings{})
 	expectedNvidiaGPUWindowsMetrics = []string{"Memory % Committed Bytes In Use", "nvidia_smi utilization_gpu", "nvidia_smi utilization_memory", "nvidia_smi power_draw", "nvidia_smi temperature_gpu"}
 )
 
 func init() {
-	environment.RegisterEnvironmentMetaDataFlags(envMetaDataStrings)
+	environment.RegisterEnvironmentMetaDataFlags(envMetaDataStringsWindows)
 }
 
 func Validate() error {
