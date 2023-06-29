047d5ef5204f79b5a59ea056c5aa45819ccbf6a2
Chena Lee <leechena@amazon.com>
2023-06-29 17:48:03 -0400

    fix cred tf bug

--- terraform/ec2/creds/variables.tf
+++ terraform/ec2/creds/variables.tf
@@ -106,3 +106,9 @@ variable "plugin_tests" {
   type    = string
   default = ""
 }
+
+variable "agent_start" {
+  description = "default command should be for ec2 with linux"
+  type        = string
+  default     = "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c "
+}
