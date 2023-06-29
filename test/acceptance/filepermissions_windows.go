c63ecd49b2e69127e521ab890374e49cde569db5
Pooja Reddy Nathala <poojardy@amazon.com>
2023-06-29 13:20:03 -0400

    changed to run with terraform

--- test/acceptance/filepermissions_windows.go
+++ test/acceptance/filepermissions_windows.go
@@ -8,48 +8,21 @@ package acceptance
 
 import (
 	"log"
-	"time"
 
 	"github.com/aws/amazon-cloudwatch-agent-test/filesystem"
-	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
 	"go.uber.org/multierr"
 )
 
 const (
-	// agent config json file in Temp dir gets written by terraform
-	configWindowsJSON       = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\agent_config.json"
-	configWindowsOutputPath = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\config.json"
-	agentWindowsLogPath     = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\Logs\\amazon-cloudwatch-agent.log"
-	agentCopiedConfigPath   = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\Configs\\file_config.json"
-	translatedTomlPath      = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent.toml"
-
-	agentWindowsRuntime = 1 * time.Minute
+	agentWindowsLogPath   = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\Logs\\amazon-cloudwatch-agent.log"
+	agentCopiedConfigPath = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\Configs\\file_config.json"
+	translatedTomlPath    = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent.toml"
 )
 
 var filePermissionsPath = []string{agentWindowsLogPath, agentCopiedConfigPath, translatedTomlPath}
 
 func Validate() error {
 	log.Printf("testing windows filepermissions")
-	err := common.CopyFile(configWindowsJSON, configWindowsOutputPath)
-	if err != nil {
-		log.Printf("Copying agent config file failed: %v", err)
-		return err
-	}
-
-	err = common.StartAgent(configWindowsOutputPath, true, false)
-	if err != nil {
-		log.Printf("Starting agent failed: %v", err)
-		return err
-	}
-
-	time.Sleep(agentWindowsRuntime)
-	log.Printf("Agent has been running for : %s", agentWindowsRuntime.String())
-
-	err = common.StopAgent()
-	if err != nil {
-		log.Printf("Stopping agent failed: %v", err)
-		return err
-	}
 
 	var multiErr error
 	for _, path := range filePermissionsPath {
