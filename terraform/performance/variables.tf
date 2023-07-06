dde5c177a3cb8685eaf6ebc610964efe0d3d4a04
Zhihong Lin <zhiholin@amazon.com>
2023-07-05 22:22:00 -0400

    Use exact metric name

--- terraform/performance/variables.tf
+++ terraform/performance/variables.tf
@@ -59,7 +59,7 @@ variable "values_per_minute" {
 
 variable "family" {
   type    = string
-  default = "windows"
+  default = "linux"
 
   validation {
     condition     = contains(["windows", "linux"], var.family)
