ce666d32d13b7ca9e56d86f23b0e6dc3986e3c3d
Zhihong Lin <zhiholin@amazon.com>
2023-07-06 02:28:37 -0400

    Add debug lines

--- validator/validators/performance/performance_validator.go
+++ validator/validators/performance/performance_validator.go
@@ -168,8 +168,11 @@ func (s *PerformanceValidator) GetPerformanceMetrics(startTime, endTime time.Tim
 				Value: aws.String(dimension.Value),
 			})
 		}
+		log.Printf("Trying to get Metric %s for GetMetricStatistic ", stat.MetricName)
 		statistics, err := awsservice.GetMetricStatistics(stat.MetricName, metricNamespace, metricDimensions, startTime, endTime, int32(s.vConfig.GetAgentCollectionPeriod().Seconds()), types.StatisticAverage)
+
 		if err != nil {
+			log.Printf("An error occurred for %s ", stat.MetricName)
 			return nil, err
 		}
 		for _, datapoint := range statistics.Datapoints {
