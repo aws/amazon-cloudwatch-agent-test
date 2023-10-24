1af68f82a0b3e139d170dad7b2212f3d80d49d20
Pooja Reddy Nathala <poojardy@amazon.com>
2023-10-24 07:43:55 -0400

    Fixed the dimensions

--- test/metric/apm_util.go
+++ test/metric/apm_util.go
@@ -20,11 +20,11 @@ var (
 
 	ServerConsumerInstructions = []dimension.Instruction{
 		{
-			Key:   "EKS.Cluster",
+			Key:   "HostedIn.EKS.Cluster",
 			Value: dimension.UnknownDimensionValue(),
 		},
 		{
-			Key:   "K8s.Namespace",
+			Key:   "HostedIn.K8s.Namespace",
 			Value: dimension.ExpectedDimensionValue{Value: aws.String("default")},
 		},
 		{
