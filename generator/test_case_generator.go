47338684409768f1c50d09eec31b8091eb7e0c63
Kulwant Singh <ikulwant@amazon.com>
2024-02-20 11:43:11 -0800

    Fix CW agent image and test generator

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -182,6 +182,27 @@ var testTypeToTestConfig = map[string][]testConfig{
 			testDir: "./test/metric_value_benchmark",
 			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
 		},
+		{
+			testDir:      "./test/metric_value_benchmark",
+			terraformDir: "terraform/eks/daemon/windows",
+			targets:      map[string]map[string]struct{}{"arc": {"amd64": {}}},
+		},
+		{
+			testDir: "./test/statsd", terraformDir: "terraform/eks/daemon/statsd",
+			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
+		},
+		{
+			testDir: "./test/emf", terraformDir: "terraform/eks/daemon/emf",
+			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
+		},
+		{
+			testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/d",
+			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
+		},
+		{testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/bit"},
+		{testDir: "./test/app_signals", terraformDir: "terraform/eks/daemon/app_signals",
+			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
+		},
 		{testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/windows"},
 	},
 	"eks_deployment": {
--- terraform/eks/daemon/windows/main.tf
+++ terraform/eks/daemon/windows/main.tf
@@ -443,7 +443,7 @@ resource "null_resource" "windows-cwagent" {
     command = <<-EOT
       curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
       chmod +x kubectl
-      sed 's+CW_TEST_IMAGE+506463145083.dkr.ecr.us-west-2.amazonaws.com/windows-container-internal:latest+' ./../default_resources/cwagent-windows.yaml | ./kubectl apply -f -
+      sed 's+CW_TEST_IMAGE+${var.cwagent_image_repo}:${var.cwagent_image_tag}+' ./../default_resources/cwagent-windows.yaml | ./kubectl apply -f -
       ./kubectl apply -f ./../default_resources/test-sample-windows.yaml
       ./kubectl rollout status daemonset cloudwatch-agent-windows -n amazon-cloudwatch --timeout 600s
       ./kubectl rollout status deployment windows-test-deployment --timeout 600s
