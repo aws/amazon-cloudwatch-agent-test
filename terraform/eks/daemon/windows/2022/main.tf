bcc13b3004df0983c16c9a13d4183afd35e2dd9c
Kulwant Singh <ikulwant@amazon.com>
2024-02-28 13:18:33 -0800

    Lint formatting

--- terraform/eks/daemon/windows/2022/main.tf
+++ terraform/eks/daemon/windows/2022/main.tf
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: MIT
 
 module "windows" {
-  source        = "../"
-  windows_ami_type = var.windows_ami_type
+  source             = "../"
+  windows_ami_type   = var.windows_ami_type
   windows_os_version = var.windows_os_version
-  test_dir = var.test_dir
-  ami_type = var.ami_type
-  instance_type = var.instance_type
-  k8s_version = var.k8s_version
+  test_dir           = var.test_dir
+  ami_type           = var.ami_type
+  instance_type      = var.instance_type
+  k8s_version        = var.k8s_version
 }
