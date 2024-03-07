9656063b1a663227b3a33c0c84460ef513b70616
Kulwant Singh <ikulwant@amazon.com>
2024-03-07 07:21:32 -0800

    Remove extra license header from terraform templates

--- terraform/eks/daemon/windows/2019/variables.tf
+++ terraform/eks/daemon/windows/2019/variables.tf
@@ -1,9 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT
-
 variable "region" {
   type    = string
   default = "us-west-2"
