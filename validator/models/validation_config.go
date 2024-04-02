78ee16be0e84e62836cae04475a88fbf8d1b80c2
siprmp <siprmp@amazon.com>
2024-04-02 12:54:38 -0400

    adding app_signals to the namespace

--- validator/models/validation_config.go
+++ validator/models/validation_config.go
@@ -15,7 +15,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var supportedReceivers = []string{"logs", "statsd", "collectd", "system", "emf", "xray"}
+var supportedReceivers = []string{"logs", "statsd", "collectd", "system", "emf", "xray", "app_signals"}
 
 type ValidateConfig interface {
 	GetPluginsConfig() []string
