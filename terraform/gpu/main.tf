1c366e659e9fe9e82c4922b20ba2db2d33d04b12
Paramadon <siprmp@amazon.com>
2024-05-20 15:44:29 -0400

    fixing test

--- terraform/gpu/main.tf
+++ terraform/gpu/main.tf
@@ -99,155 +99,18 @@ resource "aws_iam_role_policy_attachment" "node_CloudWatchAgentServerPolicy" {
   role       = aws_iam_role.node_role.name
 }
 
-# TODO: these security groups be created once and then reused
-# EKS Cluster Security Group
-resource "aws_security_group" "eks_cluster_sg" {
-  name        = "cwagent-eks-cluster-sg-${module.common.testing_id}"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = module.basic_components.vpc_id
-}
 
-resource "aws_security_group_rule" "cluster_inbound" {
-  description              = "Allow worker nodes to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_nodes_sg.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "cluster_outbound" {
-  description              = "Allow cluster API Server to communicate with the worker nodes"
-  from_port                = 1024
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_nodes_sg.id
-  to_port                  = 65535
-  type                     = "egress"
-}
-
-
-# EKS Node Security Group
-resource "aws_security_group" "eks_nodes_sg" {
-  name        = "cwagent-eks-node-sg-${module.common.testing_id}"
-  description = "Security group for all nodes in the cluster"
-  vpc_id      = module.basic_components.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group_rule" "nodes_internal" {
-  description              = "Allow nodes to communicate with each other"
-  from_port                = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.eks_nodes_sg.id
-  source_security_group_id = aws_security_group.eks_nodes_sg.id
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "nodes_cluster_inbound" {
-  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
-  from_port                = 1025
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_nodes_sg.id
-  source_security_group_id = aws_security_group.eks_cluster_sg.id
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-
-# create cert for communication between agent and dcgm
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
-
-resource "local_file" "ca_key" {
-  content  = tls_private_key.private_key.private_key_pem
-  filename = "${path.module}/certs/ca.key"
-}
-
-resource "tls_self_signed_cert" "ca_cert" {
-  private_key_pem   = tls_private_key.private_key.private_key_pem
-  is_ca_certificate = true
-  subject {
-    common_name  = "dcgm-exporter-service.amazon-cloudwatch.svc"
-    organization = "Amazon CloudWatch Agent"
-  }
-  validity_period_hours = 24
-  allowed_uses = [
-    "digital_signature",
-    "key_encipherment",
-    "cert_signing",
-    "crl_signing",
-    "server_auth",
-    "client_auth",
+resource "null_resource" "kubectl" {
+  depends_on = [
+    aws_eks_cluster.this,
+    aws_eks_node_group.this
   ]
-}
-
-resource "local_file" "ca_cert_file" {
-  content  = tls_self_signed_cert.ca_cert.cert_pem
-  filename = "${path.module}/certs/ca.cert"
-}
-
-resource "tls_private_key" "server_private_key" {
-  algorithm = "RSA"
-}
-
-resource "local_file" "server_key" {
-  content  = tls_private_key.server_private_key.private_key_pem
-  filename = "${path.module}/certs/server.key"
-}
-
-resource "tls_cert_request" "local_csr" {
-  private_key_pem = tls_private_key.server_private_key.private_key_pem
-  dns_names       = ["localhost", "127.0.0.1", "dcgm-exporter-service.amazon-cloudwatch.svc"]
-  subject {
-    common_name  = "dcgm-exporter-service.amazon-cloudwatch.svc"
-    organization = "Amazon CloudWatch Agent"
-  }
-}
-
-resource "tls_locally_signed_cert" "server_cert" {
-  cert_request_pem      = tls_cert_request.local_csr.cert_request_pem
-  ca_private_key_pem    = tls_private_key.private_key.private_key_pem
-  ca_cert_pem           = tls_self_signed_cert.ca_cert.cert_pem
-  validity_period_hours = 12
-  allowed_uses = [
-    "digital_signature",
-    "key_encipherment",
-    "server_auth",
-    "client_auth",
-  ]
-}
-
-resource "local_file" "server_cert_file" {
-  content  = tls_locally_signed_cert.server_cert.cert_pem
-  filename = "${path.module}/certs/server.cert"
-}
-
-resource "kubernetes_secret" "agent_cert" {
-  metadata {
-    name      = "amazon-cloudwatch-observability-agent-cert"
-    namespace = "amazon-cloudwatch"
-  }
-  data = {
-    "ca.crt"  = tls_self_signed_cert.ca_cert.cert_pem              #filebase64(local_file.ca_cert_file.filename)
-    "tls.crt" = tls_locally_signed_cert.server_cert.cert_pem       #filebase64(local_file.server_cert_file.filename)
-    "tls.key" = tls_private_key.server_private_key.private_key_pem #filebase64(local_file.server_key.filename)
-  }
-}
-
-
-resource "kubernetes_namespace" "namespace" {
-  metadata {
-    name = "amazon-cloudwatch"
+  provisioner "local-exec" {
+    command = <<-EOT
+      ${local.aws_eks} update-kubeconfig --name ${aws_eks_cluster.this.name}
+      ${local.aws_eks} list-clusters --output text
+      ${local.aws_eks} describe-cluster --name ${aws_eks_cluster.this.name} --output text
+    EOT
   }
 }
 
