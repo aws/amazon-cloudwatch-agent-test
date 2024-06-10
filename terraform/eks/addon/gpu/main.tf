388971052172d9f98e7d7f2d37fb68d447f0b4ab
Paramadon <siprmp@amazon.com>
2024-06-09 22:31:33 -0400

    removing patching of agent to see if that is the problem

--- terraform/eks/addon/gpu/main.tf
+++ terraform/eks/addon/gpu/main.tf
@@ -138,13 +138,6 @@ resource "null_resource" "validator" {
     command = <<EOT
       kubectl apply -f ./gpuBurner.yaml
       kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.15.0/deployments/static/nvidia-device-plugin.yml
-      kubectl patch amazoncloudwatchagents -n amazon-cloudwatch cloudwatch-agent --type='json' -p='[
-          {
-            "op": "replace",
-            "path": "/spec/image",
-            "value": "${var.aws_ecr_private_registry}/${var.ecr_integration_test_repo}:${var.github_sha}"
-          }
-        ]'
       if go test ${var.test_dir} -eksClusterName ${aws_eks_cluster.this.name} -computeType=EKS -v -eksDeploymentStrategy=DAEMON -eksGpuType=nvidia; then
         echo "Tests passed"
       else
