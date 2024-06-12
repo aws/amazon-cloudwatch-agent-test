e76ae006cbc49cd6874d5c3c8806c2680b4f13b8
Paramadon <siprmp@amazon.com>
2024-06-12 11:44:17 -0400

    adding sleep and log statements

--- test/metric/container_insights_util.go
+++ test/metric/container_insights_util.go
@@ -85,6 +85,7 @@ func getMetricsInClusterDimension(env *environment.MetaData, metricFilter string
 		},
 	}
 	metrics, err := listFetcher.Fetch(ContainerInsightsNamespace, "", dims)
+	fmt.Println(dims)
 	if err != nil {
 		log.Println("failed to fetch metric list", err)
 		return nil
@@ -97,6 +98,7 @@ func getMetricsInClusterDimension(env *environment.MetaData, metricFilter string
 	var results []dimToMetrics
 	for _, m := range metrics {
 		// filter by metric name filter
+		fmt.Println("This is the metric name and filter: ", *m.MetricName, metricFilter)
 		if metricFilter != "" && !strings.Contains(*m.MetricName, metricFilter) {
 			continue
 		}
@@ -132,6 +134,10 @@ func validateMetricsAvailability(dims string, expected []string, actual map[stri
 		Name:   dims,
 		Status: status.FAILED,
 	}
+	fmt.Println("Dims underneath")
+	fmt.Println(dims)
+	fmt.Println("This is the expected metrics: ", expected)
+	fmt.Println("This is the actual metrics: ", actual)
 	log.Printf("expected metrics: %d, actual metrics: %d", len(expected), len(actual))
 	if compareMetrics(expected, actual) {
 		testResult.Status = status.SUCCESSFUL
