0f9e41300c02b8582e34a28bfc46f0130a432785
Varun C <varunchi@amazon.com>
2024-11-11 18:56:28 +0000

    Reduce run time to 5 minutes

--- test/metric_value_benchmark/jmx_tomcat_jvm_test.go
+++ test/metric_value_benchmark/jmx_tomcat_jvm_test.go
@@ -46,7 +46,7 @@ func (t *JMXTomcatJVMTestRunner) GetAgentConfigFileName() string {
 }
 
 func (t *JMXTomcatJVMTestRunner) GetAgentRunDuration() time.Duration {
-	return 10 * time.Minute
+	return 5 * time.Minute
 }
 
 func (t *JMXTomcatJVMTestRunner) SetupBeforeAgentRun() error {
