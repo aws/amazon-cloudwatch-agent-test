43d2ca63793e44b36cbfa4b9a93e59cc3912877f
Varun C <varunchi@amazon.com>
2024-11-19 02:42:52 +0000

    Change order of tests

--- test/cloudwatchlogs/publish_logs_test.go
+++ test/cloudwatchlogs/publish_logs_test.go
@@ -214,17 +214,6 @@ func TestWriteLogsWithEntityInfo(t *testing.T) {
 				instanceId:   instanceId,
 			},
 		},
-		"IAMRole": {
-			agentConfigPath: filepath.Join("resources", "config_log.json"),
-			iterations:      1000,
-			expectedEntity: expectedEntity{
-				entityType:   "Service",
-				name:         "cwa-e2e-iam-role", //should match the name of the IAM role used in our testing
-				environment:  "ec2:default",
-				platformType: "AWS::EC2",
-				instanceId:   instanceId,
-			},
-		},
 		"ServiceInConfig": {
 			agentConfigPath: filepath.Join("resources", "config_log_service_name.json"),
 			iterations:      1000,
@@ -232,6 +221,17 @@ func TestWriteLogsWithEntityInfo(t *testing.T) {
 				entityType:   "Service",
 				name:         "service-in-config",     //should match the service.name value in the config file
 				environment:  "environment-in-config", //should match the deployment.environment value in the config file
+				platformType: "AWS::EC2",
+				instanceId:   instanceId,
+			},
+		},
+		"IAMRole": {
+			agentConfigPath: filepath.Join("resources", "config_log.json"),
+			iterations:      1000,
+			expectedEntity: expectedEntity{
+				entityType:   "Service",
+				name:         "cwa-e2e-iam-role", //should match the name of the IAM role used in our testing
+				environment:  "ec2:default",
 				platformType: "AWS::EC2",
 				instanceId:   instanceId,
 			},
