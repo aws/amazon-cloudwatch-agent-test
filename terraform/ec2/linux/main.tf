3ebf32d80a2d8163affe7781fb04125260237bbc
Kaushik Surya <kausyas@amazon.com>
2024-12-03 15:30:16 -0500

    Use mod vendor for linux tests

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -33,6 +33,7 @@ module "reboot_common" {
 locals {
   // Canary downloads latest binary. Integration test downloads binary connect to git hash.
   binary_uri = var.is_canary ? "${var.s3_bucket}/release/amazon_linux/${var.arc}/latest/${var.binary_name}" : "${var.s3_bucket}/integration-test/binary/${var.cwa_github_sha}/linux/${var.arc}/${var.binary_name}"
+  vendor_uri = "${var.s3_bucket}/integration-test/test-vendor/${var.cwa_github_sha}/vendor.tar.gz"
   // list of test that require instance reboot
   reboot_required_tests = tolist(["./test/restart"])
 }
@@ -58,6 +59,8 @@ resource "null_resource" "integration_test_setup" {
       "git clone --branch ${var.github_test_repo_branch} ${var.github_test_repo}",
       "cd amazon-cloudwatch-agent-test",
       "aws s3 cp s3://${local.binary_uri} .",
+      "aws s3 cp s3://${local.vendor_uri} ./vendor.tar.gz",
+      "tar -xzf vendor.tar.gz",
       "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
       var.install_agent,
     ]
@@ -90,6 +93,7 @@ resource "null_resource" "integration_test_run" {
       "export AWS_REGION=${var.region}",
       "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
       "go version",
+      "go mod verify",
       "echo run integration test",
       "cd ~/amazon-cloudwatch-agent-test",
       "echo run sanity test && go test ${var.use_vendor ? "-mod=vendor" : ""} ./test/sanity -p 1 -v",
