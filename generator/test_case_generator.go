f111016558430ad371ede3e4f221804205d53f13
Paramadon <siprmp@amazon.com>
2024-12-18 15:16:34 +0100

    adding fluent for windows 2025

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -229,6 +229,7 @@ var testTypeToTestConfig = map[string][]testConfig{
 		},
 		{testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/bit"},
 		{testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/windows/2022"},
+		{testDir: "./test/fluent", terraformDir: "terraform/eks/daemon/fluent/windows/2025"},
 		{
 			testDir: "./test/gpu", terraformDir: "terraform/eks/daemon/gpu",
 			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
---
+++ terraform/eks/daemon/fluent/windows/2025/main.tf
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+module "windows" {
+  source             = "../"
+  windows_ami_type   = var.windows_ami_type
+  windows_os_version = var.windows_os_version
+  test_dir           = var.test_dir
+  ami_type           = var.ami_type
+  instance_type      = var.instance_type
+  k8s_version        = var.k8s_version
+}
