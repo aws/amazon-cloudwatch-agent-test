21c2eb137878ef84b4dc290198cecb83e3e25689
musa-asad <musaasad@amazon.com>
2024-12-27 15:42:13 -0500

    add deployment strategy

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -31,6 +31,7 @@ type matrixRow struct {
 	ValuesPerMinute     int    `json:"values_per_minute"` // Number of metrics to be sent or number of log lines to write
 	K8sVersion          string `json:"k8sVersion"`
 	Nodes               int    `json:"nodes"`
+	DeploymentStrategy  string `json:"deploymentStrategy"`
 	TerraformDir        string `json:"terraform_dir"`
 	UseSSM              bool   `json:"useSSM"`
 	ExcludedTests       string `json:"excludedTests"`
