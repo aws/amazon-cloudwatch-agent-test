e1a43d7479a1b0a7c3cbf0caf6b016c0eba05652
Paramadon <siprmp@amazon.com>
2025-01-17 11:20:58 -0500

    reverting main.tf

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -86,13 +86,6 @@ resource "null_resource" "integration_test_run" {
   provisioner "remote-exec" {
     inline = [
       "echo prepare environment",
-      "sudo getenforce",
-      "sudo setenforce 1",
-      "sudo getenforce",
-      "sudo yum install -y audit --allowerasing",
-      "sudo systemctl start auditd",
-      "sudo systemctl enable auditd",
-      "sudo ausearch -m AVC,USER_AVC -ts recent",
       "export LOCAL_STACK_HOST_NAME=${var.local_stack_host_name}",
       "export AWS_REGION=${var.region}",
       "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
@@ -101,12 +94,6 @@ resource "null_resource" "integration_test_run" {
       "echo run sanity test && go test ./test/sanity -p 1 -v",
       var.pre_test_setup,
       "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v",
-      "sudo ausearch -m AVC,USER_AVC -ts recent | audit2allow -M custom_policy",
-      "cat custom_policy.te",
-      "sudo systemctl status amazon-cloudwatch-agent",
-      "sudo dmesg | grep -i avc",
-      "ps -efZ | grep amazon-cloudwatch-agent"
-
     ]
   }
 
