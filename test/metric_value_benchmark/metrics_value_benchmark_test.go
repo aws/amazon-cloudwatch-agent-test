70baaf08936a074f245c4f12e27708308f7fbee3
zhihonl <61301537+zhihonl@users.noreply.github.com>
2025-01-24 13:25:55 -0500

    Add pod identity credential integration test (#450)
    * Add pod identity credential integration test
    
    * Fix linter error
    
    * Remove fluentbit changes

--- test/metric_value_benchmark/metrics_value_benchmark_test.go
+++ test/metric_value_benchmark/metrics_value_benchmark_test.go
@@ -89,6 +89,16 @@ func getEksTestRunners(env *environment.MetaData) []*test_runner.EKSTestRunner {
 				Env: *env,
 			}
 			eksTestRunners = append(eksTestRunners, &eksDeploymentTestRunner)
+		case eksdeploymenttype.PODIDENTITY:
+			eksDaemonTestRunner := test_runner.EKSTestRunner{
+				Runner: &EKSDaemonTestRunner{BaseTestRunner: test_runner.BaseTestRunner{
+					DimensionFactory: factory,
+				},
+					env: env,
+				},
+				Env: *env,
+			}
+			eksTestRunners = append(eksTestRunners, &eksDaemonTestRunner)
 		}
 	}
 	return eksTestRunners
