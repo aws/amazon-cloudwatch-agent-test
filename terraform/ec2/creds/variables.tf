92cd7aded7a6daaaf1e88d8404a85c6c28c5fd35
Rick Rossi <dricross@amazon.com>
2025-02-07 09:05:12 -0500

    Revert terraform changes. Passing build ID in place of github sha

--- terraform/ec2/creds/variables.tf
+++ terraform/ec2/creds/variables.tf
@@ -1,23 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
-variable "agent_s3_path_linux" {
-  description = "base s3 path for the linux flavor of the agent"
-  type        = string
-  default     = ""
-}
-
-variable "agent_s3_path_mac" {
-  description = "base s3 path for the linux flavor of the agent"
-  type        = string
-  default     = ""
-}
-
-variable "agent_s3_path_windows" {
-  description = "base s3 path for the linux flavor of the agent"
-  type        = string
-  default     = ""
-}
 variable "region" {
   type    = string
   default = "us-west-2"
