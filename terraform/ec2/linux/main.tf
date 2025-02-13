2b4e9208677b010918c1ef62125224625bee6d94
Paramadon <siprmp@amazon.com>
2025-02-13 16:04:19 -0500

    adding logs

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -87,6 +87,10 @@ resource "null_resource" "integration_test_run" {
     inline = [
       "echo prepare environment",
       "sudo setenforce 0",
+      "echo enforcing mode on",
+      "sudo yum install -y audit policycoreutils-python-utils go --allowerasing",
+      "sudo systemctl start auditd",
+      "sudo systemctl enable auditd",
       "sudo rm -r amazon-cloudwatch-agent-sepolicy",
       "${var.is_selinux_test ? "sudo setenforce 0" : "echo SELinux not enforced"}",
       "${var.is_selinux_test ? "for i in {1..3}; do git clone https://github.com/Paramadon/amazon-cloudwatch-agent-sepolicy.git && break || sleep 5; done" : "echo SELinux test not enabled"}",
@@ -102,6 +106,8 @@ resource "null_resource" "integration_test_run" {
       "echo run sanity test && go test ./test/sanity -p 1 -v",
       var.pre_test_setup,
       "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v",
+      "sudo ausearch -m AVC,USER_AVC -ts recent | audit2allow -M custom_policy",
+      "cat custom_policy.te"
     ]
   }
 
