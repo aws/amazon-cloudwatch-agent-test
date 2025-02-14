22460f2dae286306d9e1690a38650c725f3c1b51
Paramadon <siprmp@amazon.com>
2025-02-14 16:44:00 -0500

    this commit is to just test if selinux_restriction works

--- test/xray_selinux_restrictions/xray_selinux_restrictions_test.go
+++ test/xray_selinux_restrictions/xray_selinux_restrictions_test.go
@@ -2,22 +2,14 @@ package xray_selinux_restrictions
 
 import (
 	"github.com/aws/amazon-cloudwatch-agent-test/environment"
-	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
 	"github.com/stretchr/testify/require"
 	"os/exec"
-	"path/filepath"
 	"testing"
 	"time"
 )
 
 func init() {
 	environment.RegisterEnvironmentMetaDataFlags()
-}
-
-func startAgent(t *testing.T) {
-	common.CopyFile(filepath.Join("agent_configs", "config.json"), common.ConfigOutputPath)
-	require.NoError(t, common.StartAgent(common.ConfigOutputPath, true, false))
-	time.Sleep(10 * time.Second) // Wait for the agent to start properly
 }
 
 // This test verifies that a customer can restrict there selinux policy
@@ -34,7 +26,7 @@ func TestXraySelinuxRestrictions(t *testing.T) {
 
 func verifyXrayTestPasses(t *testing.T) {
 	env := environment.GetEnvironmentMetaData()
-	cmd := exec.Command("go", "test", "../test/xray", "-p", "1", "-timeout", "1h", "-computeType=EC2", "-bucket="+env.Bucket,
+	cmd := exec.Command("go", "test", "../xray", "-p", "1", "-timeout", "1h", "-computeType=EC2", "-bucket="+env.Bucket,
 		"cwaCommitSha="+env.CwaCommitSha, "-caCertPath="+env.CaCertPath, "-instanceId="+env.InstanceId)
 	output, err := cmd.CombinedOutput()
 	require.NoError(t, err, "Failed xray test %s", output)
