98e6550261d46399950f60225d3b35ded360c70c
Paramadon <siprmp@amazon.com>
2025-02-17 14:30:16 -0500

    fixing issue

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -98,7 +98,6 @@ resource "null_resource" "integration_test_run" {
         "chmod +x ./amazon_cloudwatch_agent.sh",
         "sudo ./amazon_cloudwatch_agent.sh",
         "sudo systemctl restart amazon-cloudwatch-agent",
-        "cat amazon_cloudwatch_agent.te",
         "sudo chmod +x amazon_cloudwatch_agent.sh",
         "sudo ./amazon_cloudwatch_agent.sh",
         "cd .."
@@ -119,9 +118,6 @@ resource "null_resource" "integration_test_run" {
         var.pre_test_setup,
 
         # Integration test execution
-        "echo below is the either Permissive/Enforcing",
-        "getenforce",
-        "ps -efZ | grep amazon-cloudwatch-agent",
         "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v"
       ],
     )
