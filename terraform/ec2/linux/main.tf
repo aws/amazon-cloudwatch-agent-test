5088cdba5d752a90206eccab3a8d92c87ca95ab4
Varun C <varunchi@amazon.com>
2025-02-27 19:26:47 +0000

    Fix formatting

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -89,7 +89,7 @@ resource "null_resource" "download_test_repo_and_vendor_from_s3" {
   }
   provisioner "remote-exec" {
     inline = [
-      "echo Downloading cloned test repo from S3..."
+      "echo Downloading cloned test repo from S3...",
       "aws s3 sync s3://${var.s3_bucket}/integration-test/test-repo/${var.cwa_github_sha} ./amazon-cloudwatch-agent-test --delete --quiet",
       "cd amazon-cloudwatch-agent-test",
       "echo Downloading vendor directory from S3...",
@@ -101,7 +101,7 @@ resource "null_resource" "download_test_repo_and_vendor_from_s3" {
   }
 
   depends_on = [
-     module.linux_common,
+    module.linux_common,
   ]
 }
 
