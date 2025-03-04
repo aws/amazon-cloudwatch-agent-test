3cbda4c3d51354db9a120bcc0ff6a2ad5169015f
Musa <musaasad@amazon.com>
2025-03-04 14:55:59 -0500

    Add cancellation for rebooting instances during tests for all environments (#473)

--- terraform/ec2/assume_role/main.tf
+++ terraform/ec2/assume_role/main.tf
@@ -204,6 +204,7 @@ resource "null_resource" "integration_test_run" {
       "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
       "echo run integration test",
       "cd ~/amazon-cloudwatch-agent-test",
+      "nohup bash -c 'while true; do sudo shutdown -c; sleep 30; done' >/dev/null 2>&1 &",
       "echo run sanity test && go test ./test/sanity -p 1 -v",
       "echo base assume role arn is ${aws_iam_role.roles["no_context_keys"].arn}",
       "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -assumeRoleArn=${aws_iam_role.roles["no_context_keys"].arn} -instanceArn=${aws_instance.cwagent.arn} -accountId=${data.aws_caller_identity.account_id.account_id} -v"
