884122a1687f3803da581da96322e36cadc00be1
Paramadon <siprmp@amazon.com>
2025-03-10 17:00:32 -0400

    fixing selinux issue

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -120,7 +120,11 @@ resource "null_resource" "integration_test_run" {
     inline = concat(
       [
         "echo Preparing environment...",
-        "sudo setenforce 0"
+        "sudo setenforce 0",
+        "echo enforcing mode on",
+        "sudo yum install -y audit policycoreutils-python-utils go --allowerasing",
+        "sudo systemctl start auditd",
+        "sudo systemctl enable auditd",
       ],
 
       # SELinux test setup (if enabled)
@@ -128,7 +132,7 @@ resource "null_resource" "integration_test_run" {
         "sudo yum install amazon-cloudwatch-agent -y",
         "echo Running SELinux test setup...",
         "sudo yum install selinux-policy selinux-policy-targeted selinux-policy-devel -y",
-        "sudo setenforce 1",
+        "sudo setenforce 0",
         "echo below is either Permissive/Enforcing",
         "sudo getenforce",
         "sudo rm -r amazon-cloudwatch-agent-selinux",
@@ -154,6 +158,8 @@ resource "null_resource" "integration_test_run" {
         var.pre_test_setup,
         # Integration test execution
         "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v"
+        "sudo ausearch -m AVC,USER_AVC -ts recent | audit2allow -M custom_policy",
+        "cat custom_policy.te"
       ],
     )
   }
