a174cfb29aff4b4fafe9481a364073a9ee26c468
Paramadon <siprmp@amazon.com>
2025-03-11 17:57:34 -0400

    see what's the issue

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -121,6 +121,9 @@ resource "null_resource" "integration_test_run" {
       [
         "echo Preparing environment...",
         "sudo setenforce 0",
+        "sudo yum install -y audit policycoreutils-python-utils go --allowerasing",
+        "sudo systemctl start auditd",
+        "sudo systemctl enable auditd",
       ],
 
       # SELinux test setup (if enabled)
@@ -128,7 +131,7 @@ resource "null_resource" "integration_test_run" {
         "sudo yum install amazon-cloudwatch-agent -y",
         "echo Running SELinux test setup...",
         "sudo yum install selinux-policy selinux-policy-targeted selinux-policy-devel -y",
-        "sudo setenforce 1",
+        "sudo setenforce 0",
         "echo below is either Permissive/Enforcing",
         "sudo getenforce",
         "sudo rm -r amazon-cloudwatch-agent-selinux",
@@ -153,7 +156,9 @@ resource "null_resource" "integration_test_run" {
         "go test ./test/sanity -p 1 -v",
         var.pre_test_setup,
         # Integration test execution
-        "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v"
+        "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v",
+        "sudo ausearch -m AVC,USER_AVC -ts recent | audit2allow -M custom_policy",
+        "cat custom_policy.te"
       ],
     )
   }
