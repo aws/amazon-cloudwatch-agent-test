cdd0ad56c43fe3f3770805fafeddbae533a8ab81
Paramadon <siprmp@amazon.com>
2025-03-11 11:29:28 -0400

    fixing issue

--- terraform/ec2/assume_role/main.tf
+++ terraform/ec2/assume_role/main.tf
@@ -203,11 +203,14 @@ resource "null_resource" "integration_test_run" {
     inline = concat(
       [
         "echo Preparing environment...",
+        "sudo yum install -y audit policycoreutils-python-utils go --allowerasing",
+        "sudo systemctl start auditd",
+        "sudo systemctl enable auditd",
       ],
 
       # SELinux test setup (if enabled)
       var.is_selinux_test ? [
-        "sudo setenforce 1",
+        "sudo setenforce 0",
         "echo Running SELinux test setup...",
         "git clone --branch ${var.selinux_branch} https://github.com/aws/amazon-cloudwatch-agent-selinux.git",
         "cd amazon-cloudwatch-agent-selinux",
@@ -225,7 +228,9 @@ resource "null_resource" "integration_test_run" {
         "cd ~/amazon-cloudwatch-agent-test",
         "echo run sanity test && go test ./test/sanity -p 1 -v",
         "echo base assume role arn is ${aws_iam_role.roles["no_context_keys"].arn}",
-        "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -assumeRoleArn=${aws_iam_role.roles["no_context_keys"].arn} -instanceArn=${aws_instance.cwagent.arn} -accountId=${data.aws_caller_identity.account_id.account_id} -v"
+        "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -assumeRoleArn=${aws_iam_role.roles["no_context_keys"].arn} -instanceArn=${aws_instance.cwagent.arn} -accountId=${data.aws_caller_identity.account_id.account_id} -v",
+        "sudo ausearch -m AVC,USER_AVC -ts recent | audit2allow -M custom_policy",
+        "cat custom_policy.te"
       ],
     )
   }
