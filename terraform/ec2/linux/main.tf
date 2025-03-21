2f123221fe93aab5cf3948a0018cd33808f25e1e
Paramadon <parampreet0623@gmail.com>
2025-03-21 10:00:52 -0400

    fixing audit installation

--- terraform/ec2/linux/main.tf
+++ terraform/ec2/linux/main.tf
@@ -130,10 +130,8 @@ resource "null_resource" "integration_test_run" {
 
       # SELinux test setup (if enabled)
       var.is_selinux_test ? [
-        "sudo yum remove chronicle",
-        "sudo systemctl stop chronicle",
-        "sudo systemctl disable chronicle",
-        "sudo yum install audit audit-libs",
+        "sudo yum remove chronicled -y",
+        "sudo yum install audit -y",
         "sudo systemctl start auditd",
         "sudo systemctl enable auditd",
         "sudo yum install amazon-cloudwatch-agent -y",
@@ -165,7 +163,7 @@ resource "null_resource" "integration_test_run" {
         var.pre_test_setup,
         # Integration test execution
         "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -excludedTests='${var.excluded_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} ${length(regexall("/amp", var.test_dir)) > 0 ? "-ampWorkspaceId=${module.amp[0].workspace_id} " : ""}-v",
-        "sudo ausearch -m AVC,USER_AVC -ts recent -ts $(date --date='450 minutes ago' '+%H:%M:%S') | audit2allow -M custom_policysudo ausearch -m AVC,USER_AVC -ts recent -ts $(date --date='450 minutes ago' '+%H:%M:%S') | audit2allow -M custom_policy",
+        "sudo ausearch -m AVC,USER_AVC -ts recent -ts $(date --date='450 minutes ago' '+%H:%M:%S') | audit2allow -M custom_policy",
         "cat custom_policy.te"
       ],
     )
