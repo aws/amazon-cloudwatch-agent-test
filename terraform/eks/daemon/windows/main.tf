d47ead6cc12a7ac38c7c197974fca506d00dee7e
musa-asad <musaasad@amazon.com>
2025-03-23 15:22:32 -0400

    increase timeout

--- terraform/eks/daemon/windows/main.tf
+++ terraform/eks/daemon/windows/main.tf
@@ -446,7 +446,7 @@ resource "null_resource" "windows-cwagent" {
       sed 's+CW_TEST_IMAGE+${var.cwagent_image_repo}:${var.cwagent_image_tag}+' ./../../default_resources/cwagent-windows-daemonset.yaml | ./kubectl apply -f -
       sed -e 's+WINDOWS_SERVER_VERSION+${var.windows_os_version}+' -e 's+REPLICAS+1+' ./../../default_resources/test-sample-windows.yaml | ./kubectl apply -f -
       ./kubectl rollout status daemonset cloudwatch-agent-windows -n amazon-cloudwatch --timeout 600s
-      ./kubectl rollout status deployment windows-test-deployment --timeout 600s
+      ./kubectl rollout status deployment windows-test-deployment --timeout 1200s
     EOT
   }
 
