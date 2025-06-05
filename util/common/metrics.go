5e952efac99afc9197051130ede0dc36a6923c11
Paramadon <parampreet0623@gmail.com>
2025-06-04 20:48:32 -0400

    fixing scrape

--- util/common/metrics.go
+++ util/common/metrics.go
@@ -214,9 +214,9 @@ type PrometheusConfig struct {
 func createPrometheusConfig(scrapeInterval int) error {
 	log.Printf("[Prometheus] Creating config with scrape interval: %ds", scrapeInterval)
 
-	// Replace $SCRAPE_INTERVAL with the actual value
-	cfg := strings.ReplaceAll(prometheusTemplate, "$SCRAPE_INTERVAL", fmt.Sprintf("%ds", scrapeInterval))
-	cfg = strings.ReplaceAll(prometheusTemplate, "$PORT", fmt.Sprintf("%d", 8101))
+	cfg := prometheusTemplate
+	cfg = strings.ReplaceAll(cfg, "SCRAPE_INTERVAL", fmt.Sprintf("%ds", scrapeInterval))
+	cfg = strings.ReplaceAll(cfg, "$PORT", fmt.Sprintf("%d", 8101))
 
 	log.Printf("[Prometheus] Writing config to /tmp/prometheus.yaml:\n%s", cfg)
 
