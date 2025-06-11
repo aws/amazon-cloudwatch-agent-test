acb9a9f2b5ea055938e52928dd6e76e7f46c4817
Michael Commey <michaelcommey@icloud.com>
2025-06-11 10:08:28 -0400

    eventId integ test

--- util/common/logs.go
+++ util/common/logs.go
@@ -34,9 +34,16 @@ func GenerateLogs(configFilePath string, duration time.Duration, sendingInterval
 
 func GenerateWindowsEvents(validationLog []models.LogValidation) error {
 	var multiErr error
+
 	for _, vLog := range validationLog {
+
 		if vLog.LogSource == "WindowsEvents" && vLog.LogLevel != "" {
-			err := CreateWindowsEvent(vLog.LogStream, vLog.LogLevel, vLog.LogValue)
+			id := "1"
+			if vLog.LogEventID != "" {
+				id = vLog.LogEventID
+			}
+
+			err := CreateWindowsEvent(vLog.LogStream, vLog.LogLevel, id, vLog.LogValue)
 			if err != nil {
 				multiErr = multierr.Append(multiErr, err)
 			}
@@ -45,15 +52,16 @@ func GenerateWindowsEvents(validationLog []models.LogValidation) error {
 	return multiErr
 }
 
-func CreateWindowsEvent(eventLogName string, eventLogLevel string, msg string) error {
-	out, err := exec.Command("eventcreate", "/ID", "1", "/L", eventLogName, "/T", eventLogLevel, "/SO", "MYEVENTSOURCE"+eventLogName, "/D", msg).Output()
+func CreateWindowsEvent(eventLogName string, eventLogLevel string, eventID string, msg string) error {
+
+	out, err := exec.Command("eventcreate", "/ID", eventID, "/L", eventLogName, "/T", eventLogLevel, "/SO", "MYEVENTSOURCE"+eventLogName, "/D", msg).Output()
 
 	if err != nil {
 		log.Printf("Windows event creation failed: %v; the output is: %s", err, string(out))
 		return err
 	}
 
-	log.Printf("Windows Event is successfully created for logname: %s, loglevel: %s, logmsg: %s", eventLogName, eventLogLevel, msg)
+	log.Printf("Windows Event is successfully created for logname: %s, loglevel: %s, logeventId: %s, logmsg: %s", eventLogName, eventLogLevel, eventID, msg)
 	return nil
 }
 
--- validator/models/validation_config.go
+++ validator/models/validation_config.go
@@ -67,11 +67,12 @@ type MetricValidation struct {
 }
 
 type LogValidation struct {
-	LogValue  string `yaml:"log_value"`
-	LogLines  int    `yaml:"log_lines"`
-	LogStream string `yaml:"log_stream"`
-	LogLevel  string `yaml:"log_level"`
-	LogSource string `yaml:"log_source"`
+	LogValue   string `yaml:"log_value"`
+	LogLines   int    `yaml:"log_lines"`
+	LogStream  string `yaml:"log_stream"`
+	LogLevel   string `yaml:"log_level"`
+	LogEventID string `yaml:"log_event_id"`
+	LogSource  string `yaml:"log_source"`
 }
 
 type MetricDimension struct {
