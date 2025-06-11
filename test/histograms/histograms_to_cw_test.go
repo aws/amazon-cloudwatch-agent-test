8fadd8bff4bd17fc1cac5483b53b3b92fdb33f51
Paramadon <parampreet0623@gmail.com>
2025-06-11 13:27:46 -0400

    lowering time

--- test/histograms/histograms_to_cw_test.go
+++ test/histograms/histograms_to_cw_test.go
@@ -89,7 +89,7 @@ func (t OtlpHistogramTestRunner) Validate() status.TestGroupResult {
 }
 
 func (t *OtlpHistogramTestRunner) GetAgentRunDuration() time.Duration {
-	return 5 * time.Minute
+	return 2 * time.Minute
 }
 
 func (t OtlpHistogramTestRunner) GetMeasuredMetrics() []string {
