36a7d8e9648ccb5e1d919404417e5c49cffe1be6
Akansha Agarwal <agarakan@users.noreply.github.com>
2025-06-19 18:10:10 +0000

    Update validation logic

--- test/ecs/ecs_sd/ecs_servicediscovery_runner.go
+++ test/ecs/ecs_sd/ecs_servicediscovery_runner.go
@@ -1,27 +1,28 @@
 package ecs_sd
 
 import (
+	_ "embed"
 	"fmt"
 	"github.com/aws/amazon-cloudwatch-agent-test/environment"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/status"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/test_runner"
 	"github.com/aws/amazon-cloudwatch-agent-test/util/awsservice"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
-	"github.com/stretchr/testify/assert"
 	"log"
 	"strings"
 	"time"
 )
 
 const (
-	RetryTime = 15
+	MaxRetryCount = 15
 	// Log group format: https://github.com/aws/amazon-cloudwatch-agent/blob/5ef3dba446cb56a4c2306878592b5d14300ae82f/translator/translate/otel/exporter/awsemf/prometheus.go#L38
 	ECSLogGroupNameFormat = "/aws/ecs/containerinsights/%s/prometheus"
 	// Log stream based on job name in extra_apps.tpl:https://github.com/aws/amazon-cloudwatch-agent-test/blob/main/test/ecs/ecs_sd/resources/extra_apps.tpl#L41
 	LogStreamName = "prometheus-redis"
-
-	namespace = "ecs_servicediscovery" //todo
 )
+
+//go:embed resources/emf_prometheus_redis_schema.json
+var schema string
 
 type ECSServiceDiscoveryTestRunner struct {
 	test_runner.BaseTestRunner
@@ -40,9 +41,8 @@ func (t ECSServiceDiscoveryTestRunner) GetMeasuredMetrics() []string {
 }
 
 func (t ECSServiceDiscoveryTestRunner) Validate() status.TestGroupResult {
-	testResults := []status.TestResult{}
-	testResults = append(testResults, t.validateHistogramMetric("my.delta.histogram")...)
-	testResults = append(testResults, t.validateHistogramMetric("my.cumulative.histogram")...)
+	var testResults []status.TestResult
+	testResults = append(testResults, t.ValidateCloudWatchLogs())
 
 	return status.TestGroupResult{
 		Name:        t.GetTestName(),
@@ -50,41 +50,49 @@ func (t ECSServiceDiscoveryTestRunner) Validate() status.TestGroupResult {
 	}
 }
 
-func TestValidatingCloudWatchLogs(t *testing.T) {
+func (t ECSServiceDiscoveryTestRunner) ValidateCloudWatchLogs() status.TestResult {
 	env := environment.GetEnvironmentMetaData()
-	logGroupName, logGroupFound, start, end := ValidateLogGroupFormat(t, env)
-
-	ValidateLogsContent(t, logGroupName, start, end)
-
-	if logGroupFound {
-		awsservice.DeleteLogGroupAndStream(logGroupName, LogStreamName)
-	}
-}
-
-func ValidateLogGroupFormat(t *testing.T, env *environment.MetaData) (string, bool, time.Time, time.Time) {
-	start := time.Now()
 	logGroupName := fmt.Sprintf(ECSLogGroupNameFormat, env.EcsClusterName)
 
-	var logGroupFound bool
-	for currentRetry := 1; ; currentRetry++ {
-
-		if currentRetry == RetryTime {
-			t.Fatalf("Test has exhausted %v retry time", RetryTime)
-		}
-
-		if !awsservice.IsLogGroupExists(logGroupName) {
-			log.Printf("Current retry: %v/%v and begin to sleep for 20s \n", currentRetry, RetryTime)
-			time.Sleep(20 * time.Second)
-			continue
-		}
-		break
+	testResult := status.TestResult{
+		Name:   fmt.Sprintf("Retrieve Test LogGroup: %s", logGroupName),
+		Status: status.FAILED,
 	}
-	end := time.Now()
-	return logGroupName, logGroupFound, start, end
+
+	logGroupFound, err := t.ValidateLogGroupFormat(logGroupName)
+
+	if logGroupFound {
+		if err != nil {
+			log.Printf("ECS ServiceDiscovery Test LogGroups invalid\n")
+			testResult.Name = err.Error()
+			testResult.Status = status.FAILED
+		} else {
+			testResult.Status = status.SUCCESSFUL
+		}
+		awsservice.DeleteLogGroupAndStream(logGroupName, LogStreamName)
+	}
+	return testResult
 }
 
-func ValidateLogsContent(t *testing.T, logGroupName string, start time.Time, end time.Time) {
-	err := awsservice.ValidateLogs(
+func (t ECSServiceDiscoveryTestRunner) ValidateLogGroupFormat(logGroupName string) (bool, error) {
+	start := time.Now()
+
+	for retries := 0; retries < MaxRetryCount; retries++ {
+		if awsservice.IsLogGroupExists(logGroupName) {
+			end := time.Now()
+			return true, t.ValidateLogsContent(logGroupName, start, end)
+		}
+
+		log.Printf("Retry %d/%d: Log group not found. Waiting 20 seconds...\n", retries+1, MaxRetryCount)
+		time.Sleep(20 * time.Second)
+	}
+
+	log.Printf("ECS ServiceDiscovery Test has exhausted %v retry times", MaxRetryCount)
+	return false, fmt.Errorf("%s:%s", "Test Retries Exhausted ", MaxRetryCount)
+}
+
+func (t ECSServiceDiscoveryTestRunner) ValidateLogsContent(logGroupName string, start time.Time, end time.Time) error {
+	return awsservice.ValidateLogs(
 		logGroupName,
 		LogStreamName,
 		&start,
@@ -102,5 +110,4 @@ func ValidateLogsContent(t *testing.T, logGroupName string, start time.Time, end
 			awsservice.AssertLogContainsSubstring("\"job\":\"prometheus-redis\""),
 		),
 	)
-	assert.NoError(t, err)
 }
