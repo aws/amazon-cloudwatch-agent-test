0997a1b58f9767e177507309e85405b028c9de0c
Paramadon <parampreet0623@gmail.com>
2025-06-20 11:26:44 -0400

    attempting to fix eks tests

--- terraform/eks/daemon/providers.tf
+++ terraform/eks/daemon/providers.tf
@@ -1,6 +1,28 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.36.0"  # Pinning to this specific version
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "aws" {
   region = var.region
 }
@@ -14,26 +36,4 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.this.token
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.36.0"  # Pin to this specific version
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-  }
-  required_version = ">= 1.0"  # Add this line to specify minimum Terraform version
 }
