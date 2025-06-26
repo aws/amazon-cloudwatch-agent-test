4d88873a5da73d4b4eed314fab022ccb1342c8ef
Paramadon <parampreet0623@gmail.com>
2025-06-26 16:13:57 -0400

    fixing lint

--- test/metric_value_benchmark/prometheus_pmd_test.go
+++ test/metric_value_benchmark/prometheus_pmd_test.go
@@ -1,18 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
 package metric_value_benchmark
 
 import (
 	_ "embed"
 	"fmt"
-	"github.com/aws/amazon-cloudwatch-agent-test/util/awsservice"
 	"log"
 	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric/dimension"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/status"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/test_runner"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/awsservice"
 	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
-	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 type PrometheusPMDTestRunner struct {
