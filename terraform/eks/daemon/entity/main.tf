858aadb5c211cc0965cbbd46c94342a209d535de
Hyunsoo Kim <884273+movence@users.noreply.github.com>
2025-06-30 16:30:59 -0400

    fix helm struct failures and AMI for eks 1.33

--- terraform/eks/daemon/entity/main.tf
+++ terraform/eks/daemon/entity/main.tf
@@ -181,16 +181,16 @@ resource "helm_release" "aws_observability" {
   chart            = "./helm-charts/charts/amazon-cloudwatch-observability"
   namespace        = "amazon-cloudwatch"
   create_namespace = true
-
-  set {
-    name  = "clusterName"
-    value = aws_eks_cluster.this.name
-  }
-
-  set {
-    name  = "region"
-    value = "us-west-2"
-  }
+  set = [
+    {
+      name  = "clusterName"
+      value = aws_eks_cluster.this.name
+    }, 
+    {
+      name  = "region"
+      value = "us-west-2"
+    }
+  ]
   depends_on = [
     aws_eks_cluster.this,
     aws_eks_node_group.this,
