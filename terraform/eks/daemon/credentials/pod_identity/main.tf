58e13ae287539598bea7d2bebf06c8cb2b788c7d
Hyunsoo Kim <hsookim@amazon.com>
2025-07-02 08:53:35 -0400

    fmt

--- terraform/eks/daemon/credentials/pod_identity/main.tf
+++ terraform/eks/daemon/credentials/pod_identity/main.tf
@@ -217,7 +217,7 @@ resource "helm_release" "aws_observability" {
     {
       name  = "clusterName"
       value = aws_eks_cluster.this.name
-    }, 
+    },
     {
       name  = "region"
       value = "us-west-2"
