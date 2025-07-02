1b1b103c0829dd2bf7740c5c4efbc4cc7e7a278a
Jason Polanco <polajaso@amazon.com>
2025-07-02 13:40:13 -0400

    typos

--- terraform/eks/performance/providers.tf
+++ terraform/eks/performance/providers.tf
@@ -20,6 +20,11 @@ provider "helm" {
   kubernetes = {
     host                   = aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
-    token = data.aws_eks_cluster_auth.this.token
+
+    exec = {
+        api_version = "client.authentication.k8s.io/v1beta1"
+        command     = "aws"
+        args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+    }
   }
 }
