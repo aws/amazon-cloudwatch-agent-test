98f05a75a33465b0542848d3a5892bff472b3f8a
Jason Polanco <polajaso@amazon.com>
2025-07-10 18:13:18 -0400

    remove toek from kubernetes

--- terraform/eks/performance/providers.tf
+++ terraform/eks/performance/providers.tf
@@ -13,7 +13,6 @@ provider "kubernetes" {
   }
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
