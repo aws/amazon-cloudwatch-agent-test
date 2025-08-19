27d18d50183d5d4891832907663b28e5a3fc3526
Kaushik Surya <kausyas@amazon.com>
2025-08-19 17:06:04 -0400

    Better error logging for EKS CI test failures

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -344,7 +344,7 @@ var testTypeToTestConfig = map[string][]testConfig{
 		{
 			testDir:      "./test/ebscsi",
 			terraformDir: "terraform/eks/daemon/ebs",
-			targets: map[string]map[string]struct{}{"arc": {"amd64": {}}},
+			targets:      map[string]map[string]struct{}{"arc": {"amd64": {}}},
 		},
 	},
 	"eks_deployment": {
