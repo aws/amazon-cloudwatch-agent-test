54e171bb238f2b902d55b4ecb7b53db9b9207591
Hyunsoo Kim <hsookim@amazon.com>
2025-09-09 09:49:29 -0400

    consolidate tests

--- test/otlp/otlp_test.go
+++ test/otlp/otlp_test.go
@@ -24,21 +24,108 @@ import (
 )
 
 const (
-	testRuntime = 4 * time.Minute
-	namespace   = "CWAgent/OTLP"
-	logGroup    = "/aws/amazon-cloudwatch-agent/otlp"
+	testRuntime           = 4 * time.Minute
+	namespace             = "CWAgent/OTLP"
+	logGroup              = "/aws/amazon-cloudwatch-agent/otlp"
+	agentRuntime          = 5 * time.Minute
+	loadGeneratorInterval = 5 * time.Second
 )
 
 func init() {
 	environment.RegisterEnvironmentMetaDataFlags()
 }
 
-type UnifiedOTLPTestRunner struct {
+//func TestTraces(t *testing.T) {
+//	env := environment.GetEnvironmentMetaData()
+//	testCases := map[string]struct {
+//		agentConfigPath string
+//		generatorConfig *base.TraceGeneratorConfig
+//	}{
+//		"WithOTLP/Simple": {
+//			agentConfigPath: filepath.Join("resources", "otlp-config.json"),
+//			generatorConfig: &base.TraceGeneratorConfig{
+//				Interval: loadGeneratorInterval,
+//				Annotations: map[string]interface{}{
+//					"test_type":   "simple_otlp",
+//					"instance_id": env.InstanceId,
+//					"commit_sha":  env.CwaCommitSha,
+//				},
+//				Metadata: map[string]map[string]interface{}{
+//					"default": {
+//						"custom_key": "custom_value",
+//					},
+//				},
+//				Attributes: []attribute.KeyValue{
+//					attribute.String("custom_key", "custom_value"),
+//					attribute.String("test_type", "simple_otlp"),
+//					attribute.String("instance_id", env.InstanceId),
+//					attribute.String("commit_sha", env.CwaCommitSha),
+//				},
+//			},
+//		},
+//		"WithOTLP/Enhanced": {
+//			agentConfigPath: filepath.Join("resources", "otlp-traces-config.json"),
+//			generatorConfig: &base.TraceGeneratorConfig{
+//				Interval: loadGeneratorInterval,
+//				Annotations: map[string]interface{}{
+//					"test_type":     "enhanced_otlp",
+//					"instance_id":   env.InstanceId,
+//					"commit_sha":    env.CwaCommitSha,
+//					"service_name":  "otlp-trace-test",
+//					"operation":     "test_operation",
+//					"trace_version": "v2.0",
+//				},
+//				Metadata: map[string]map[string]interface{}{
+//					"default": {
+//						"custom_key":    "custom_value",
+//						"test_metadata": "enhanced_test",
+//						"environment":   "integration_test",
+//					},
+//					"http": {
+//						"method": "POST",
+//						"url":    "/api/test",
+//						"status": 200,
+//					},
+//				},
+//				Attributes: []attribute.KeyValue{
+//					attribute.String("custom_key", "custom_value"),
+//					attribute.String("test_type", "enhanced_otlp"),
+//					attribute.String("instance_id", env.InstanceId),
+//					attribute.String("commit_sha", env.CwaCommitSha),
+//					attribute.String("service.name", "otlp-trace-test"),
+//					attribute.String("service.version", "1.0.0"),
+//					attribute.String("http.method", "POST"),
+//					attribute.String("http.url", "/api/test"),
+//					attribute.Int("http.status_code", 200),
+//					attribute.Bool("test.enabled", true),
+//				},
+//			},
+//		},
+//	}
+//	t.Logf("Sanity check: number of test cases:%d", len(testCases))
+//	for name, testCase := range testCases {
+//
+//		t.Run(name, func(t *testing.T) {
+//
+//			OtlpTestCfg := base.TraceTestConfig{
+//				Generator:       otlp.NewLoadGenerator(testCase.generatorConfig),
+//				Name:            name,
+//				AgentConfigPath: testCase.agentConfigPath,
+//				AgentRuntime:    agentRuntime,
+//			}
+//			err := base.TraceTest(t, OtlpTestCfg)
+//			require.NoError(t, err, "TraceTest failed because %s", err)
+//
+//		})
+//	}
+//}
+
+type OtlpTestRunner struct {
 	test_runner.BaseTestRunner
 	env *environment.MetaData
 }
 
-func (t *UnifiedOTLPTestRunner) Validate() status.TestGroupResult {
+func (t *OtlpTestRunner) Validate() status.TestGroupResult {
 	var results []status.TestResult
 
 	// Validate traces
@@ -58,7 +145,7 @@ func (t *UnifiedOTLPTestRunner) Validate() status.TestGroupResult {
 	}
 }
 
-func (t *UnifiedOTLPTestRunner) validateTraces() status.TestResult {
+func (t *OtlpTestRunner) validateTraces() status.TestResult {
 	annotations := map[string]interface{}{
 		"test_type":   "unified_otlp",
 		"instance_id": t.env.InstanceId,
@@ -71,7 +158,7 @@ func (t *UnifiedOTLPTestRunner) validateTraces() status.TestResult {
 	return status.TestResult{Name: "OTLP_Traces", Status: status.SUCCESSFUL}
 }
 
-func (t *UnifiedOTLPTestRunner) validateMetric(metricName string) status.TestResult {
+func (t *OtlpTestRunner) validateMetric(metricName string) status.TestResult {
 	instructions := []dimension.Instruction{{
 		Key:   "InstanceId",
 		Value: dimension.ExpectedDimensionValue{Value: &t.env.InstanceId},
@@ -101,7 +188,7 @@ func (t *UnifiedOTLPTestRunner) validateMetric(metricName string) status.TestRes
 	return testResult
 }
 
-func (t *UnifiedOTLPTestRunner) validateLogs() status.TestResult {
+func (t *OtlpTestRunner) validateLogs() status.TestResult {
 	testResult := status.TestResult{
 		Name:   "OTLP_Logs",
 		Status: status.FAILED,
@@ -133,14 +220,14 @@ func (t *UnifiedOTLPTestRunner) validateLogs() status.TestResult {
 	return testResult
 }
 
-func (t *UnifiedOTLPTestRunner) GetTestName() string                { return "UnifiedOTLP" }
-func (t *UnifiedOTLPTestRunner) GetAgentRunDuration() time.Duration { return testRuntime }
-func (t *UnifiedOTLPTestRunner) GetMeasuredMetrics() []string {
+func (t *OtlpTestRunner) GetTestName() string                { return "UnifiedOTLP" }
+func (t *OtlpTestRunner) GetAgentRunDuration() time.Duration { return testRuntime }
+func (t *OtlpTestRunner) GetMeasuredMetrics() []string {
 	return []string{"otlp_counter", "otlp_gauge"}
 }
-func (t *UnifiedOTLPTestRunner) GetAgentConfigFileName() string { return "unified-otlp-config.json" }
+func (t *OtlpTestRunner) GetAgentConfigFileName() string { return "unified-otlp-config.json" }
 
-func (t *UnifiedOTLPTestRunner) SetupAfterAgentRun() error {
+func (t *OtlpTestRunner) SetupAfterAgentRun() error {
 	// Start trace generation
 	go t.generateTraces()
 
@@ -178,7 +265,7 @@ func (t *UnifiedOTLPTestRunner) SetupAfterAgentRun() error {
 	return common.RunAsyncCommand(cmd)
 }
 
-func (t *UnifiedOTLPTestRunner) generateTraces() {
+func (t *OtlpTestRunner) generateTraces() {
 	generator := otlp.NewLoadGenerator(&base.TraceGeneratorConfig{
 		Interval: 15 * time.Second,
 		Annotations: map[string]interface{}{
@@ -193,12 +280,12 @@ func (t *UnifiedOTLPTestRunner) generateTraces() {
 	generator.StartSendingTraces(context.Background())
 }
 
-var _ test_runner.ITestRunner = (*UnifiedOTLPTestRunner)(nil)
+var _ test_runner.ITestRunner = (*OtlpTestRunner)(nil)
 
 func TestUnifiedOTLP(t *testing.T) {
 	env := environment.GetEnvironmentMetaData()
 
-	testRunner := &UnifiedOTLPTestRunner{env: env}
+	testRunner := &OtlpTestRunner{env: env}
 	runner := &test_runner.TestRunner{TestRunner: testRunner}
 
 	result := runner.Run()
