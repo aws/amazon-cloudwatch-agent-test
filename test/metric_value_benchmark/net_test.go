b2cb428eef31f32ea29c2fdbe60e68a59c359bcf
Varun C <varunchi@amazon.com>
2025-09-26 18:10:47 +0000

    reset net_test.go

--- test/metric_value_benchmark/net_test.go
+++ test/metric_value_benchmark/net_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/amazon-cloudwatch-agent-test/test/metric/dimension"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/status"
 	"github.com/aws/amazon-cloudwatch-agent-test/test/test_runner"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
 )
 
 type NetTestRunner struct {
@@ -34,10 +35,10 @@ func (m *NetTestRunner) Validate() status.TestGroupResult {
 }
 
 func (m *NetTestRunner) SetupBeforeAgentRun() error {
-	// err := common.RunCommands([]string{"ping -c 10 127.0.0.1 > /dev/null 2>&1 &"})
-	// if err != nil {
-	// 	return err
-	// }
+	err := common.RunCommands([]string{"sudo systemctl restart docker"})
+	if err != nil {
+		return err
+	}
 	return m.SetUpConfig()
 }
 
@@ -64,7 +65,7 @@ func (m *NetTestRunner) validateNetMetric(metricName string) status.TestResult {
 	dims, failed := m.DimensionFactory.GetDimensions([]dimension.Instruction{
 		{
 			Key:   "interface",
-			Value: dimension.ExpectedDimensionValue{aws.String("eth0")},
+			Value: dimension.ExpectedDimensionValue{aws.String("docker0")},
 		},
 		{
 			Key:   "InstanceId",
