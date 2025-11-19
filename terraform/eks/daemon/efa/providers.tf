fdda2dcca162046f95cc4dbba5da426ed9815c34
Marcus Mann <mpmann@amazon.com>
2025-11-19 16:32:53 -0500

    Convert EFA test to use terraform-aws-modules/eks v21.0 with CloudWatch Observability addon

--- terraform/eks/daemon/efa/providers.tf
+++ terraform/eks/daemon/efa/providers.tf
@@ -18,10 +18,10 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
@@ -30,10 +30,10 @@ provider "helm" {
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
