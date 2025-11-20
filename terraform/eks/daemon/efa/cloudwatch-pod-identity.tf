c48cefabb88a37366caeebe93f3531967d693a37
Marcus Mann <mpmann@amazon.com>
2025-11-19 21:56:26 -0500

    add pod identity associations

--- terraform/eks/daemon/efa/cloudwatch-pod-identity.tf
+++ terraform/eks/daemon/efa/cloudwatch-pod-identity.tf
@@ -32,3 +32,29 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_observability_server" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   role       = aws_iam_role.cloudwatch_observability.name
 }
+
+# Pod Identity association for CloudWatch agent
+resource "aws_eks_pod_identity_association" "cloudwatch_agent" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "amazon-cloudwatch"
+  service_account = "cloudwatch-agent"
+  role_arn        = aws_iam_role.cloudwatch_observability.arn
+
+  tags = {
+    Name  = "cloudwatch-agent-${module.common.testing_id}"
+    Owner = "default"
+  }
+}
+
+# Pod Identity association for Fluent Bit
+resource "aws_eks_pod_identity_association" "fluent_bit" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "amazon-cloudwatch"
+  service_account = "fluent-bit"
+  role_arn        = aws_iam_role.cloudwatch_observability.arn
+
+  tags = {
+    Name  = "fluent-bit-${module.common.testing_id}"
+    Owner = "default"
+  }
+}
