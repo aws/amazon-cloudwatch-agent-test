f94eb4e640a1edef4d5eceaff903b241c6eaf280
Marcus Mann <mpmann@amazon.com>
2025-11-20 08:58:02 -0500

    add providers

--- terraform/eks/daemon/efa/providers.tf
+++ terraform/eks/daemon/efa/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
