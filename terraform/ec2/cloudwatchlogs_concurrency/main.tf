923d9eb61c38e31597105eb14aca5f245dfb801f
Marcus Mann <mpmann@amazon.com>
2026-02-17 17:01:01 -0500

    style: terraform fmt

--- terraform/ec2/cloudwatchlogs_concurrency/main.tf
+++ terraform/ec2/cloudwatchlogs_concurrency/main.tf
@@ -62,9 +62,9 @@ resource "aws_iam_role_policy" "cwagent_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "DenyRestrictedLogGroups"
-        Effect = "Deny"
-        Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Sid      = "DenyRestrictedLogGroups"
+        Effect   = "Deny"
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
         Resource = "arn:aws:logs:*:*:log-group:aws-restricted-log-group-name-*"
       },
       {
