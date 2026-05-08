45d2d18e0ea7b8eb48c3fb2ec0fb07b0d26b4393
Spandan Naik <spanaik@amazon.com>
2026-05-08 23:22:46 +0100

    Add OTEL efa cluster: EFA device and ENI correlation tests (#680)

--- generator/test_case_generator.go
+++ generator/test_case_generator.go
@@ -437,6 +437,13 @@ var testTypeToTestConfig = map[string][]testConfig{
 			terraformDir: "terraform/eks/daemon/otel-ebs-csi",
 			targets:      map[string]map[string]struct{}{"arc": {"amd64": {}}},
 		},
+		{
+			testDir:      "./test/otel/efa",
+			terraformDir: "terraform/eks/daemon/otel-efa",
+			targets:      map[string]map[string]struct{}{"arc": {"amd64": {}}},
+			instanceType: "c5n.9xlarge",
+			ami:          "AL2023_x86_64_STANDARD",
+		},
 	},
 	"eks_deployment": {
 		{testDir: "./test/metric_value_benchmark"},
