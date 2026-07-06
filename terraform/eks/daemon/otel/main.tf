e00a84bc83b2c2d48ded236a6d38d1ccc7bee6d6
Vaishnavi Joshi <vaijo@amazon.com>
2026-07-06 14:52:54 +0100

    fix : renamed flag name in main.tf

--- terraform/eks/daemon/otel/main.tf
+++ terraform/eks/daemon/otel/main.tf
@@ -344,7 +344,7 @@ resource "helm_release" "aws_observability" {
     { name = "clusterName", value = aws_eks_cluster.this.name },
     { name = "region", value = var.region },
     { name = "otelContainerInsights.enabled", value = "true" },
-    { name = "otelContainerInsights.metrics.karpenter.enabled", value = "true" },
+    { name = "otelContainerInsights.integrations.karpenter.enabled", value = "true" },
   ]
 
   depends_on = [
