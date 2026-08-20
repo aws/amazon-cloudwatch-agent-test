81579acc059efd56af2e84e5473ece958a2580b1
Michael Commey <137405771+Paamicky@users.noreply.github.com>
2026-08-20 11:16:49 -0400

    Add OTLP Windows Tests (#747)

---
+++ test/otel_collect/windows/host_metrics/host_metrics_windows.go
@@ -0,0 +1,52 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+//go:build windows
+
+package host_metrics
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/amazon-cloudwatch-agent-test/environment"
+	"github.com/aws/amazon-cloudwatch-agent-test/test/otel_collect/otlpvalidation"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/otelmetrics"
+)
+
+//go:embed resources/config.json
+var testConfigJSON string
+
+const (
+	tmpConfigPath = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\config.json"
+	runtime       = 3 * time.Minute
+)
+
+func Validate() error {
+	env := environment.GetEnvironmentMetaData()
+
+	if err := os.WriteFile(tmpConfigPath, []byte(testConfigJSON), 0644); err != nil {
+		return fmt.Errorf("could not write config: %w", err)
+	}
+	if err := common.CopyFile(tmpConfigPath, common.ConfigOutputPath); err != nil {
+		return fmt.Errorf("could not copy config: %w", err)
+	}
+	if err := common.StartAgent(common.ConfigOutputPath, true, false); err != nil {
+		return fmt.Errorf("could not start agent: %w", err)
+	}
+	time.Sleep(runtime)
+	_ = common.StopAgent()
+
+	return otelmetrics.AssertMetricsPresent(
+		context.Background(),
+		env.Region,
+		[]string{"system.cpu.utilization", "system.memory.utilization", "system.network.io", "system.disk.operations"},
+		otlpvalidation.ResourceHostIDLabels(env.InstanceId),
+		3,
+		30*time.Second,
+	)
+}
---
+++ test/otel_collect/windows/prometheus/prometheus_windows.go
@@ -0,0 +1,69 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+//go:build windows
+
+package prometheus
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/amazon-cloudwatch-agent-test/environment"
+	"github.com/aws/amazon-cloudwatch-agent-test/test/otel_collect/otlpvalidation"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/common"
+	"github.com/aws/amazon-cloudwatch-agent-test/util/otelmetrics"
+)
+
+//go:embed resources/config.json
+var testConfigJSON string
+
+//go:embed resources/prometheus_scrape.yaml
+var scrapeConfig string
+
+//go:embed resources/prometheus_metrics
+var prometheusMetrics string
+
+const (
+	tmpConfigPath = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\config.json"
+	scrapePath    = "C:\\prometheus_scrape.yaml"
+	exporterPort  = 8101
+	runtime       = 3 * time.Minute
+)
+
+func Validate() error {
+	env := environment.GetEnvironmentMetaData()
+
+	stop, err := common.StartPrometheusFakeServer(exporterPort, prometheusMetrics)
+	if err != nil {
+		return fmt.Errorf("could not start fake prometheus exporter: %w", err)
+	}
+	defer stop()
+
+	if err := os.WriteFile(scrapePath, []byte(scrapeConfig), 0644); err != nil {
+		return fmt.Errorf("could not write scrape config: %w", err)
+	}
+	if err := os.WriteFile(tmpConfigPath, []byte(testConfigJSON), 0644); err != nil {
+		return fmt.Errorf("could not write agent config: %w", err)
+	}
+	if err := common.CopyFile(tmpConfigPath, common.ConfigOutputPath); err != nil {
+		return fmt.Errorf("could not copy config: %w", err)
+	}
+	if err := common.StartAgent(common.ConfigOutputPath, true, false); err != nil {
+		return fmt.Errorf("could not start agent: %w", err)
+	}
+	time.Sleep(runtime)
+	_ = common.StopAgent()
+
+	return otelmetrics.AssertMetricsPresent(
+		context.Background(),
+		env.Region,
+		[]string{"node_cpu_seconds_total", "node_memory_MemAvailable_bytes"},
+		otlpvalidation.ResourceHostIDLabels(env.InstanceId),
+		3,
+		30*time.Second,
+	)
+}
